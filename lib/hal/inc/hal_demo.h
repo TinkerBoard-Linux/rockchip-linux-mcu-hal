3538832f5cc372ba89812366263df5e1f9df6499
Jon Lin <jon.lin@rock-chips.com>
2019-01-03 01:51:10 -0800

    lib: hal: add HAL demo
    Change-Id: I8313aa52a663c9178ed416ad6a1714658e393333
    Signed-off-by: Jon Lin <jon.lin@rock-chips.com>

---
+++ lib/hal/inc/hal_demo.h
@@ -0,0 +1,56 @@
+/* SPDX-License-Identifier: BSD-3-Clause */
+/*
+ * Copyright (c) 2019 Rockchip Electronic Co.,Ltd
+ */
+
+/**
+ * @addtogroup RKMCU_HAL_Driver
+ *  @{
+ */
+
+/** @addtogroup DEMO
+ *  @{
+ */
+
+#ifndef _HAL_DEMO_H_
+#define _HAL_DEMO_H_
+
+#include "hal_def.h"
+
+/***************************** MACRO Definition ******************************/
+/** @defgroup DEMO_Exported_Definition_Group1 Basic Definition
+ *  @{
+ */
+
+/** Add brief to here */
+#define DEMO_TEST_DEFINE 0
+
+/***************************** Structure Definition **************************/
+
+/**
+ *  Add multi line brief to here
+ *  ...
+ */
+typedef enum
+{
+    TEST_ENUM_ONE,
+    TEST_ENUM_TWO,
+    TEST_ENUM_THREE
+} eDEMO_Test;
+
+/** @} */
+/***************************** Function Declare ******************************/
+
+HAL_Status HAL_DEMO_Supsend(void);
+HAL_Status HAL_DEMO_Resume(void);
+HAL_Status HAL_DEMO_GetXXXState(void);
+HAL_Status HAL_DEMO_WriteByte_DMA(void);
+HAL_Status HAL_DEMO_Init(void);
+HAL_Status HAL_DEMO_Deinit(void);
+HAL_Status HAL_DEMO_CommonFunction(void);
+
+#endif
+
+/** @} */
+
+/** @} */
---
+++ lib/hal/src/hal_demo.c
@@ -0,0 +1,186 @@
+/* SPDX-License-Identifier: BSD-3-Clause */
+/*
+ * Copyright (c) 2019 Rockchip Electronic Co.,Ltd
+ */
+
+/**
+ *  @addtogroup RKMCU_HAL_Driver
+ *  @{
+ */
+
+/** @addtogroup DEMO
+ *  @{
+ */
+
+/** @defgroup How_To_Use How To Use
+ @verbatim
+
+ ==============================================================================
+                    #### How to use ####
+ ==============================================================================
+ The DEMO driver can be used as follows:
+
+ @endverbatim
+ */
+
+#include "hal_base.h"
+
+#ifdef HAL_DEMO_MODULE_ENABLED
+
+/********************* Private MACRO Definition ******************************/
+
+/********************* Private Structure Definition **************************/
+
+/********************* Private Variable Definition ***************************/
+
+/********************* Private Function Definition ***************************/
+
+/********************* Public Function Definition ****************************/
+/** @defgroup DEMO_Exported_Functions_Group1 Suspend and Resume Functions
+ @verbatim
+
+ ===============================================================================
+             #### Suspend and Resume functions ####
+ ===============================================================================
+ This section provides functions allowing to suspend and resume the module:
+
+ ...to do or delete this row
+
+ @endverbatim
+ *  @{
+ */
+
+/**
+ * @brief  It is for test.
+ * @param  inputTest: for test.
+ * @return HAL_Status
+ * ...to do or delete this row
+ */
+HAL_Status HAL_DEMO_Supsend(uint32_t inputTest)
+{
+    /* ...to do */
+}
+
+/**
+ * @brief  It is for test.
+ * @param  inputTest: for test.
+ * @return HAL_Status
+ * ...to do or delete this row
+ */
+HAL_Status HAL_DEMO_Resume(uint32_t inputTest)
+{
+    /* ...to do */
+}
+
+/** @} */
+
+/** @defgroup DEMO_Exported_Functions_Group2 State and Errors Functions
+ @verbatim
+
+ ===============================================================================
+             #### State and Errors functions ####
+ ===============================================================================
+ This section provides functions allowing to get the status of the module:
+
+ @endverbatim
+ *  @{
+ */
+
+/**
+ * @brief  It is for test.
+ * @param  inputTest: for test.
+ * @return HAL_Status
+ * ...to do or delete this row
+ */
+HAL_Status HAL_DEMO_GetXXXState(uint32_t inputTest)
+{
+    /* ...to do */
+}
+
+/** @} */
+
+/** @defgroup DEMO_Exported_Functions_Group3 IO Functions
+ @verbatim
+
+ ===============================================================================
+             #### IO functions ####
+ ===============================================================================
+ This section provides functions allowing to IO controlling:
+
+ @endverbatim
+ *  @{
+ */
+
+/**
+ * @brief  It is for test.
+ * @param  inputTest: for test.
+ * @return HAL_Status
+ * ...to do or delete this row
+ */
+HAL_Status HAL_DEMO_WriteByte_DMA(uint32_t inputTest)
+{
+    /* ...to do */
+}
+
+/** @} */
+
+/** @defgroup DEMO_Exported_Functions_Group4 Init and Deinit Functions
+ @verbatim
+
+ ===============================================================================
+             #### Init and deinit functions ####
+ ===============================================================================
+ This section provides functions allowing to init and deinit the module:
+
+ ...to do or delete this row
+
+ @endverbatim
+ *  @{
+ */
+
+/**
+ * @brief  It is for test.
+ * @param  inputTest: for test.
+ * @return HAL_Status
+ * ...to do or delete this row
+ */
+HAL_Status HAL_DEMO_Init(uint32_t inputTest)
+{
+    /* ...to do */
+}
+
+/**
+ * @brief  It is for test.
+ * @param  inputTest: for test.
+ * @return HAL_Status
+ * ...to do or delete this row
+ */
+HAL_Status HAL_DEMO_Deinit(uint32_t inputTest)
+{
+    /* ...to do */
+}
+
+/** @} */
+
+/** @defgroup DEMO_Exported_Functions_Group5 Other Functions
+ *  @{
+ */
+
+/**
+ * @brief  It is for test.
+ * @param  inputTest: for test.
+ * @return HAL_Status
+ * ...to do or delete this row
+ */
+HAL_Status HAL_DEMO_CommonFunction(uint32_t inputTest)
+{
+    /* ...to do */
+}
+
+/** @} */
+
+#endif
+
+/** @} */
+
+/** @} */
