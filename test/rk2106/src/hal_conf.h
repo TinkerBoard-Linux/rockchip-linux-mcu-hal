1a6d7a681674121d7be7a06bc40a82ab8cc8c4cd
Jon Lin <jon.lin@rock-chips.com>
2019-01-08 00:58:33 -0800

    test: rk2106: rm all including header files
    separate HAL module macro definitions from header files
    
    Change-Id: If48f30ba43793842e9fd3c3d11d6af815b5f15a8
    Signed-off-by: Jon Lin <jon.lin@rock-chips.com>

--- test/rk2106/src/hal_conf.h
+++ test/rk2106/src/hal_conf.h
@@ -15,35 +15,15 @@
 #define HAL_DBG_WRN_ON 1
 #define HAL_DBG_ERR_ON 1
 
+/* HAL SOC Config */
+#define RKMCU_RK2106
+
+/* HAL Driver Config */
 #define HAL_CRU_MODULE_ENABLED
 #define HAL_GPIO_MODULE_ENABLED
 #define HAL_GRF_MODULE_ENABLED
 #define HAL_NVIC_MODULE_ENABLED
 #define HAL_TIMER_MODULE_ENABLED
 #define HAL_UART_MODULE_ENABLED
-
-#ifdef HAL_CRU_MODULE_ENABLED
-#include "hal_cru.h"
-#endif
-
-#ifdef HAL_GPIO_MODULE_ENABLED
-#include "hal_gpio.h"
-#endif
-
-#ifdef HAL_GPIO_MODULE_ENABLED
-#include "hal_grf.h"
-#endif
-
-#ifdef HAL_NVIC_MODULE_ENABLED
-#include "hal_nvic.h"
-#endif
-
-#ifdef HAL_TIMER_MODULE_ENABLED
-#include "hal_timer.h"
-#endif
-
-#ifdef HAL_UART_MODULE_ENABLED
-#include "hal_uart.h"
-#endif
 
 #endif
