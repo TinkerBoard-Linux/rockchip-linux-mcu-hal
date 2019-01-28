f9dbf0fee0b756a802339b05803221c4a91158e0
Jon Lin <jon.lin@rock-chips.com>
2019-01-28 15:07:40 +0800

    lib: hal: add HAL_DBG_HEX to hal_debug
    print continuous HEX data
    
    Change-Id: I9fa48b210ff35d064a485bedd0f7873c696c1e0f
    Signed-off-by: Jon Lin <jon.lin@rock-chips.com>

--- lib/hal/src/hal_debug.c
+++ lib/hal/src/hal_debug.c
@@ -42,6 +42,61 @@ __weak void AssertFailed(const char *file, uint32_t line)
 
 /********************* Public Function Definition ****************************/
 
+/** @defgroup TIMER_Exported_Functions_Group4 Init and Deinit Functions
+ @verbatim
+
+ ===============================================================================
+             #### Init and deinit functions ####
+ ===============================================================================
+ This section provides functions allowing to init and deinit module as follows:
+
+ @endverbatim
+ *  @{
+ */
+
+/**
+ * @brief  format hex print.
+ * @param  s: head tag for every new line.
+ * @param  buf: buffer for printing.
+ * @param  width: every single printed object width.
+ * @param  len: the number of printed objects.
+ * @return HAL_Status: HAL_OK.
+ * sum = width * len (BYTE).
+ */
+HAL_Status HAL_DBG_HEX(uint8_t *s, void *buf, uint32_t width, uint32_t len)
+{
+#if HAL_DBG_ON
+    uint32_t i, j;
+    unsigned char *p8 = (unsigned char *)buf;
+    unsigned short *p16 = (unsigned short *)buf;
+    uint32_t *p32 = (uint32_t *)buf;
+
+    j = 0;
+    for (i = 0; i < len; i++) {
+        if (j == 0) {
+            HAL_SYSLOG("[HAL_DBG_HEX] %s %p + 0x%lx:", s, buf, i * width);
+        }
+
+        if (width == 4)
+            HAL_SYSLOG("0x%08lx,", p32[i]);
+        else if (width == 2)
+            HAL_SYSLOG("0x%04x,", p16[i]);
+        else
+            HAL_SYSLOG("0x%02x,", p8[i]);
+
+        if (++j >= 16) {
+            j = 0;
+            HAL_SYSLOG("\n");
+        }
+    }
+    HAL_SYSLOG("\n");
+#endif
+
+    return HAL_OK;
+}
+
+/** @} */
+
 /** @} */
 
 /** @} */
