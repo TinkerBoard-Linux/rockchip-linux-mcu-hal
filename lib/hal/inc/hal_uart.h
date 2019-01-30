15d057be29d9872ae88625f8cb4e200e2c15332e
Huibin Hong <huibin.hong@rock-chips.com>
2019-01-30 20:36:36 +0800

    lib: hal: update uart hal
    Change-Id: I5026bd513f90d16a21d5034ae3056c21aa780639
    Signed-off-by: Huibin Hong <huibin.hong@rock-chips.com>

--- lib/hal/inc/hal_uart.h
+++ lib/hal/inc/hal_uart.h
@@ -10,24 +10,124 @@
 
 /***************************** MACRO Definition ******************************/
 
-typedef enum
-{
-    UART_DEV0,
-    UART_DEV1,
-    UART_DEV2,
-    UART_DEV3,
-    UART_DEV4,
-    UART_DEV5,
-    UART_DEV_MAX
-} eUART_DEV;
+/* Out: Interrupt Enable Register */
+#define UART_IER_PTIME 0x80 /* Enable Programmable THRE Interrupt Mode */
+#define UART_IER_MSI 0x08 /* Enable Modem status interrupt */
+#define UART_IER_RLSI 0x04 /* Enable receiver line status interrupt */
+#define UART_IER_THRI 0x02 /* Enable Transmitter holding register int. */
+#define UART_IER_RDI 0x01 /* Enable receiver data interrupt */
 
-typedef enum
-{
-    UART_CHA,
-    UART_CHB,
-    UART_CHC,
-    UART_CH_MAX
-} eUART_CH;
+/* In:  Interrupt ID Register */
+#define UART_IIR_MASK 0x0f /* Interrupt ID Mask */
+#define UART_IIR_NO_INT 0x01 /* No interrupts pending */
+#define UART_IIR_ID 0x0e /* Mask for the interrupt ID */
+#define UART_IIR_MSI 0x00 /* Modem status interrupt */
+#define UART_IIR_THRI 0x02 /* Transmitter holding register empty */
+#define UART_IIR_RDI 0x04 /* Receiver data interrupt */
+#define UART_IIR_RLSI 0x06 /* Receiver line status interrupt */
+#define UART_IIR_BUSY 0x07 /* DesignWare APB Busy Detect */
+#define UART_IIR_RX_TIMEOUT 0x0c /* RX Timeout interrupt */
+
+/* Out: FIFO Control Register */
+#define UART_FCR_ENABLE_FIFO 0x01 /* Enable the FIFO */
+#define UART_FCR_CLEAR_RCVR 0x02 /* Clear the RCVR FIFO */
+#define UART_FCR_CLEAR_XMIT 0x04 /* Clear the XMIT FIFO */
+#define UART_FCR_DMA_SELECT 0x08 /* For DMA applications */
+
+#define UART_FCR_R_TRIG_00 0x00
+#define UART_FCR_R_TRIG_01 0x40
+#define UART_FCR_R_TRIG_10 0x80
+#define UART_FCR_R_TRIG_11 0xc0
+#define UART_FCR_T_TRIG_00 0x00
+#define UART_FCR_T_TRIG_01 0x10
+#define UART_FCR_T_TRIG_10 0x20
+#define UART_FCR_T_TRIG_11 0x30
+
+#define UART_FCR_TRIGGER_MASK 0xC0 /* Mask for the FIFO trigger range */
+#define UART_FCR_TRIGGER_1 0x00 /* Mask for trigger set at 1 */
+#define UART_FCR_TRIGGER_4 0x40 /* Mask for trigger set at 4 */
+#define UART_FCR_TRIGGER_8 0x80 /* Mask for trigger set at 8 */
+#define UART_FCR_TRIGGER_14 0xC0 /* Mask for trigger set at 14 */
+/* 16650 definitions */
+#define UART_FCR6_R_TRIGGER_8 0x00 /* Mask for receive trigger set at 1 */
+#define UART_FCR6_R_TRIGGER_16 0x40 /* Mask for receive trigger set at 4 */
+#define UART_FCR6_R_TRIGGER_24 0x80 /* Mask for receive trigger set at 8 */
+#define UART_FCR6_R_TRIGGER_28 0xC0 /* Mask for receive trigger set at 14 */
+#define UART_FCR6_T_TRIGGER_16 0x00 /* Mask for transmit trigger set at 16 */
+#define UART_FCR6_T_TRIGGER_8 0x10 /* Mask for transmit trigger set at 8 */
+#define UART_FCR6_T_TRIGGER_24 0x20 /* Mask for transmit trigger set at 24 */
+#define UART_FCR6_T_TRIGGER_30 0x30 /* Mask for transmit trigger set at 30 */
+#define UART_FCR7_64BYTE \
+    0x20 /* Go into 64 byte mode (TI16C750 and
+                    some Freescale UARTs) */
+
+#define UART_FCR_R_TRIG_SHIFT 6
+#define UART_FCR_R_TRIG_BITS(x) \
+    (((x)&UART_FCR_TRIGGER_MASK) >> UART_FCR_R_TRIG_SHIFT)
+#define UART_FCR_R_TRIG_MAX_STATE 4
+
+/* Out: Line Control Register */
+/*
+ * Note: if the word length is 5 bits (UART_LCR_WLEN5), then setting
+ * UART_LCR_STOP will select 1.5 stop bits, not 2 stop bits.
+ */
+#define UART_LCR_DLAB 0x80 /* Divisor latch access bit */
+#define UART_LCR_SBC 0x40 /* Set break control */
+#define UART_LCR_SPAR 0x20 /* Stick parity (?) */
+#define UART_LCR_EPAR 0x10 /* Even parity select */
+#define UART_LCR_PARITY 0x08 /* Parity Enable */
+#define UART_LCR_STOP 0x04 /* Stop bits: 0=1 bit, 1=2 bits */
+#define UART_LCR_WLEN5 0x00 /* Wordlength: 5 bits */
+#define UART_LCR_WLEN6 0x01 /* Wordlength: 6 bits */
+#define UART_LCR_WLEN7 0x02 /* Wordlength: 7 bits */
+#define UART_LCR_WLEN8 0x03 /* Wordlength: 8 bits */
+
+/* Out: Modem Control Register */
+#define UART_MCR_CLKSEL 0x80 /* Divide clock by 4 (TI16C752, EFR[4]=1) */
+#define UART_MCR_TCRTLR 0x40 /* Access TCR/TLR (TI16C752, EFR[4]=1) */
+#define UART_MCR_XONANY 0x20 /* Enable Xon Any (TI16C752, EFR[4]=1) */
+#define UART_MCR_AFE 0x20 /* Enable auto-RTS/CTS (TI16C550C/TI16C750) */
+#define UART_MCR_LOOP 0x10 /* Enable loopback test mode */
+#define UART_MCR_OUT2 0x08 /* Out2 complement */
+#define UART_MCR_OUT1 0x04 /* Out1 complement */
+#define UART_MCR_RTS 0x02 /* RTS complement */
+#define UART_MCR_DTR 0x01 /* DTR complement */
+
+/* In:  Line Status Register */
+#define UART_LSR_FIFOE 0x80 /* Fifo error */
+#define UART_LSR_TEMT 0x40 /* Transmitter empty */
+#define UART_LSR_THRE 0x20 /* Transmit-hold-register empty */
+#define UART_LSR_BI 0x10 /* Break interrupt indicator */
+#define UART_LSR_FE 0x08 /* Frame error indicator */
+#define UART_LSR_PE 0x04 /* Parity error indicator */
+#define UART_LSR_OE 0x02 /* Overrun error indicator */
+#define UART_LSR_DR 0x01 /* Receiver data ready */
+#define UART_LSR_BRK_ERROR_BITS 0x1E /* BI, FE, PE, OE bits */
+
+/* In:  Modem Status Register */
+#define UART_MSR_DCD 0x80 /* Data Carrier Detect */
+#define UART_MSR_RI 0x40 /* Ring Indicator */
+#define UART_MSR_DSR 0x20 /* Data Set Ready */
+#define UART_MSR_CTS 0x10 /* Clear to Send */
+#define UART_MSR_DDCD 0x08 /* Delta DCD */
+#define UART_MSR_TERI 0x04 /* Trailing edge ring indicator */
+#define UART_MSR_DDSR 0x02 /* Delta DSR */
+#define UART_MSR_DCTS 0x01 /* Delta CTS */
+#define UART_MSR_ANY_DELTA 0x0F /* Any of the delta bits! */
+
+#define UART_USR_RX_FIFO_FULL 0x10 /* Receive FIFO full */
+#define UART_USR_RX_FIFO_NOT_EMPTY 0x08 /* Receive FIFO not empty */
+#define UART_USR_TX_FIFO_EMPTY 0x04 /* Transmit FIFO empty */
+#define UART_USR_TX_FIFO_NOT_FULL 0x02 /* Transmit FIFO not full */
+#define UART_USR_BUSY 0x01 /* UART busy indicator */
+
+#define UART_SRR_UR 0x1 /* UART Reset */
+#define UART_SRR_RFR 0X2 /* RCVR FIFO Reset */
+#define UART_SRR_XFR 0x4 /* XMIT FIFO Reset */
+
+#define MODE_X_DIV 16 /* baud = f / 16 / div */
+
+/***************************** Structure Definition **************************/
 
 typedef enum
 {
@@ -46,7 +146,12 @@ typedef enum
     UART_BR_230400 = 230400,
     UART_BR_380400 = 380400,
     UART_BR_460800 = 460800,
-    UART_BR_921600 = 921600
+    UART_BR_921600 = 921600,
+    UART_BR_1000000 = 1000000,
+    UART_BR_1500000 = 1500000,
+    UART_BR_2000000 = 2000000,
+    UART_BR_3000000 = 3000000,
+    UART_BR_4000000 = 4000000,
 } eUART_BaudRate;
 
 typedef enum
@@ -78,20 +183,25 @@ typedef enum
     UART_INT_NUM
 } UART_INT_TYPE;
 
-/***************************** Structure Definition **************************/
-
 /***************************** Function Declare ******************************/
-
-void HAL_UART_SetIntEnabled(eUART_CH uartPort, uint32_t uartIntNumb);
-void HAL_UART_SetIntDisabled(eUART_CH uartPort, uint32_t uartIntNumb);
-int HAL_UART_WriteByte(eUART_CH uartPort, uint8_t *pdata, uint32_t cnt);
-int HAL_UART_WriteByte(eUART_CH uartPort, uint8_t *pdata, uint32_t cnt);
-int HAL_UART_ReadByte(eUART_CH uartPort, uint8_t *pdata, uint32_t cnt);
-HAL_Status HAL_UART_Init(eUART_CH uartPort, eUART_BaudRate baudRate,
+void HAL_UART_EnableIrq(struct UART_REG *pReg, uint32_t uartIntNumb);
+void HAL_UART_DisableIrq(struct UART_REG *pReg, uint32_t uartIntNumb);
+void HAL_UART_EnableLoopback(struct UART_REG *pReg);
+void HAL_UART_DisableLoopback(struct UART_REG *pReg);
+void HAL_UART_EnableAutoFlowControl(struct UART_REG *pReg);
+void HAL_UART_DisableAutoFlowControl(struct UART_REG *pReg);
+uint32_t HAL_UART_GetIrqID(struct UART_REG *pReg);
+uint32_t HAL_UART_GetLsr(struct UART_REG *pReg);
+uint32_t HAL_UART_GetUsr(struct UART_REG *pReg);
+uint32_t HAL_UART_GetMsr(struct UART_REG *pReg);
+void HAL_UART_SerialOutChar(struct UART_REG *pReg, uint8_t c);
+int HAL_UART_SerialOut(struct UART_REG *pReg, uint8_t *pdata, uint32_t cnt);
+int HAL_UART_SerialIn(struct UART_REG *pReg, uint8_t *pdata, uint32_t cnt);
+HAL_Status HAL_UART_HandleIrq(struct UART_REG *pReg);
+HAL_Status HAL_UART_Init(struct UART_REG *pReg, eUART_BaudRate baudRate,
                          eUART_dataLen dataBit, eUART_stopBit stopBit,
                          eUART_parityEn parity);
-uint32_t HAL_UART_GetIntType(eUART_CH uartPort);
-uint32_t HAL_UART_GetCTSState(eUART_CH uartPort);
-void HAL_UART_EnableAutoFlowControl(eUART_CH uartPort);
-
+HAL_Status HAL_UART_DeInit(struct UART_REG *pReg);
+HAL_Status HAL_UART_Suspend(struct UART_REG *pReg);
+HAL_Status HAL_UART_Resume(struct UART_REG *pReg);
 #endif
--- lib/hal/src/hal_uart.c
+++ lib/hal/src/hal_uart.c
@@ -3,146 +3,280 @@
  * Copyright (c) 2018 Rockchip Electronic Co.,Ltd
  */
 
+/** @addtogroup RKMCU_HAL_Driver
+ *  @{
+ */
+
+/** @addtogroup UART
+ *  @{
+ */
+
+/** @defgroup How_To_Use How To Use
+ *  @{
+ @verbatim
+
+ ==============================================================================
+                    #### How to use ####
+ ==============================================================================
+ The UART driver can be used as follows:
+
+ @endverbatim
+ @} */
+
 #include "hal_base.h"
 
 #ifdef HAL_UART_MODULE_ENABLED
 
 /********************* Private MACRO Definition ******************************/
-#define UART_PORT(n) ((struct UART_REG *)(UART_GROUP[n]))
-
 /********************* Private Structure Definition **************************/
-static const uint32_t UART_GROUP[6] = {
-    UART0_BASE, UART1_BASE, UART2_BASE, UART3_BASE, UART4_BASE, UART5_BASE,
-};
-
 /********************* Private Variable Definition ***************************/
+
 /********************* Private Function Definition ***************************/
-static void UART_Rest(eUART_CH uartPort)
+static void UART_Reset(struct UART_REG *pReg)
 {
-    struct UART_REG *pReg = UART_PORT(uartPort);
-    pReg->UART_SRR = UART_RESET | RCVR_FIFO_REST | XMIT_FIFO_RESET;
+    pReg->UART_SRR = UART_SRR_UR | UART_SRR_RFR | UART_SRR_XFR;
     pReg->UART_IER = 0;
     pReg->UART_DMASA = 1;
 }
 
-static int32_t UART_SetBaudRate(eUART_CH uartPort, uint32_t clk,
+static void UART_EnableDLAB(struct UART_REG *pReg)
+{
+    pReg->UART_LCR |= UART_LCR_DLAB;
+}
+
+static void UART_DisableDLAB(struct UART_REG *pReg)
+{
+    pReg->UART_LCR &= ~(UART_LCR_DLAB);
+}
+
+static int32_t UART_SetBaudRate(struct UART_REG *pReg, uint32_t clk,
                                 uint32_t baudRate)
 {
     uint32_t DivLatch;
-    struct UART_REG *pReg = UART_PORT(uartPort);
 
     DivLatch = clk / MODE_X_DIV / baudRate;
 
-    pReg->UART_LCR |= LCR_DLA_EN;
+    pReg->UART_MCR |= UART_MCR_LOOP;
+    UART_EnableDLAB(pReg);
 
-    pReg->UART_DLL = (DivLatch >> 0) & 0xff;
+    pReg->UART_DLL = DivLatch & 0xff;
     pReg->UART_DLH = (DivLatch >> 8) & 0xff;
 
-    pReg->UART_LCR &= (~LCR_DLA_EN);
+    UART_DisableDLAB(pReg);
+    pReg->UART_MCR &= ~(UART_MCR_LOOP);
 
     return (0);
 }
 
-static int32_t UART_SetLcrReg(eUART_CH uartPort, uint8_t byteSize,
+static int32_t UART_SetLcrReg(struct UART_REG *pReg, uint8_t byteSize,
                               uint8_t parity, uint8_t stopBits)
 {
-    uint32_t lcr;
+    uint32_t lcr = 0;
     int32_t bRet = 0;
-
-    struct UART_REG *pReg = UART_PORT(uartPort);
-    lcr = (pReg->UART_LCR) & (~(uint32_t)(UART_DATABIT_MASK));
 
     switch (byteSize) {
     case UART_DATA_5B:
-        lcr |= LCR_WLS_5;
+        lcr |= UART_LCR_WLEN5;
         break;
     case UART_DATA_6B:
-        lcr |= LCR_WLS_6;
+        lcr |= UART_LCR_WLEN6;
         break;
     case UART_DATA_7B:
-        lcr |= LCR_WLS_7;
+        lcr |= UART_LCR_WLEN7;
         break;
     case UART_DATA_8B:
-        lcr |= LCR_WLS_8;
+        lcr |= UART_LCR_WLEN8;
         break;
     default:
         bRet = -1;
         break;
     }
 
-    //Parity set
     switch (parity) {
     case UART_ODD_PARITY:
     case UART_EVEN_PARITY:
-        lcr |= PARITY_ENABLED;
+        lcr |= UART_LCR_PARITY;
         lcr |= ((parity) << 4);
         break;
     case UART_PARITY_DISABLE:
-        lcr &= ~PARITY_ENABLED;
+        lcr &= ~UART_LCR_PARITY;
         break;
     default:
         bRet = -1;
         break;
     }
 
-    //StopBits set
-    switch (stopBits) {
-    case 0:
-        lcr |= ONE_STOP_BIT;
-        break;
-    case 1:
-        lcr |= ONE_HALF_OR_TWO_BIT;
-        break;
-    default:
-        bRet = -1;
-        break;
-    }
+    if (stopBits == UART_ONE_AND_HALF_OR_TWO_STOPBIT)
+        lcr |= UART_LCR_STOP;
 
-    if (bRet == 0) {
-        pReg->UART_LCR = lcr;
-    }
-
+    pReg->UART_LCR = lcr;
     return (bRet);
 }
 
-static void UART_SetFifoEnabledNumb(eUART_CH uartPort, uint32_t param)
-{
-    struct UART_REG *pReg = UART_PORT(uartPort);
-
-    pReg->UART_SFE = (param & 0x01);
-    pReg->UART_SRT = (param >> 1) & 0x03;
-    pReg->UART_STET = (param >> 3) & 0x03;
-}
-
 /********************* Public Function Definition ****************************/
-/**
-  * @brief  UART enable interrupt
-  * @param  uartPort: uart port
-  * @param  uartIntNumb: uart irq num
-  * @return HAL_Status
-  */
-void HAL_UART_SetIntEnabled(eUART_CH uartPort, uint32_t uartIntNumb)
-{
-    struct UART_REG *pReg = UART_PORT(uartPort);
 
+/** @defgroup UART_Exported_Functions_Group1 control uart Functions
+ @verbatim
+
+ ===============================================================================
+             #### control uart functions ####
+ ===============================================================================
+ This section provides functions allowing to control uart:
+
+ @endverbatim
+ *  @{
+ */
+/**
+  * @brief  enable uart sub interrupt
+  * @param  pReg: uart reg base
+  * @param  uartIntNumb: uart irq num, such as UART_IER_RDI
+  * @return none
+  */
+void HAL_UART_EnableIrq(struct UART_REG *pReg, uint32_t uartIntNumb)
+{
     pReg->UART_IER |= uartIntNumb;
 }
 
-void HAL_UART_SetIntDisabled(eUART_CH uartPort, uint32_t uartIntNumb)
+/**
+  * @brief  disable uart sub interrupt
+  * @param  pReg: uart reg base
+  * @param  uartIntNumb: uart irq num, such as UART_IER_RDI
+  * @return none
+  */
+void HAL_UART_DisableIrq(struct UART_REG *pReg, uint32_t uartIntNumb)
 {
-    struct UART_REG *pReg = UART_PORT(uartPort);
-
-    pReg->UART_IER &= (~uartIntNumb);
+    pReg->UART_IER &= ~uartIntNumb;
 }
 
-int HAL_UART_WriteByte(eUART_CH uartPort, uint8_t *pdata, uint32_t cnt)
+/**
+  * @brief  enable uart loop back mode
+  * @param  pReg: uart reg base
+  * @return none
+  */
+void HAL_UART_EnableLoopback(struct UART_REG *pReg)
 {
-    int dwRealSize;
-    struct UART_REG *pReg = UART_PORT(uartPort);
+    pReg->UART_MCR |= UART_MCR_LOOP;
+}
 
-    dwRealSize = 0;
+/**
+  * @brief  disable uart loop back mode
+  * @param  pReg: uart reg base
+  * @return none
+  */
+void HAL_UART_DisableLoopback(struct UART_REG *pReg)
+{
+    pReg->UART_MCR &= ~(UART_MCR_LOOP);
+}
+
+/**
+  * @brief  enable uart hardware auto flow control
+  * @param  pReg: uart reg base
+  * @return none
+  */
+void HAL_UART_EnableAutoFlowControl(struct UART_REG *pReg)
+{
+    pReg->UART_MCR = UART_MCR_AFE | 0X02;
+}
+
+/**
+  * @brief  disable uart hardware auto flow control
+  * @param  pReg: uart reg base
+  * @return none
+  */
+void HAL_UART_DisableAutoFlowControl(struct UART_REG *pReg)
+{
+    pReg->UART_MCR &= ~UART_MCR_AFE;
+}
+
+/** @defgroup UART_Exported_Functions_Group2 get uart status Functions
+ @verbatim
+
+ ===============================================================================
+             #### get uart status functions ####
+ ===============================================================================
+ This section provides functions allowing to get uart status:
+
+ @endverbatim
+ *  @{
+ */
+
+/**
+  * @brief  get uart sub interrupt number
+  * @param  pReg: uart reg base
+  * @return irq number like UART_IIR_RDI
+  */
+uint32_t HAL_UART_GetIrqID(struct UART_REG *pReg)
+{
+    return (pReg->UART_IIR & UART_IIR_MASK);
+}
+
+/**
+  * @brief  get uart sub interrupt number
+  * @param  pReg: uart reg base
+  * @return line status
+  */
+uint32_t HAL_UART_GetLsr(struct UART_REG *pReg)
+{
+    return pReg->UART_LSR;
+}
+
+/**
+  * @brief  get uart status
+  * @param  pReg: uart reg base
+  * @return uart status
+  */
+uint32_t HAL_UART_GetUsr(struct UART_REG *pReg)
+{
+    return pReg->UART_USR;
+}
+
+/**
+  * @brief  get uart modem status
+  * @param  pReg: uart reg base
+  * @return uart modem status
+  */
+uint32_t HAL_UART_GetMsr(struct UART_REG *pReg)
+{
+    return pReg->UART_MSR;
+}
+
+/** @} */
+
+/** @defgroup UART_Exported_Functions_Group3 IO Functions
+ @verbatim
+
+ ===============================================================================
+             #### IO functions ####
+ ===============================================================================
+ This section provides functions allowing to IO controlling:
+
+ @endverbatim
+ *  @{
+ */
+/**
+  * @brief  send one character
+  * @param  pReg: uart reg base
+  * @param  c: the character to be sent
+  * @return none
+  */
+void HAL_UART_SerialOutChar(struct UART_REG *pReg, uint8_t c)
+{
+    pReg->UART_THR = c;
+}
+
+/**
+  * @brief  send many characters
+  * @param  pReg: uart reg base
+  * @param  pdata: characters buffer
+  * @param  cnt: the number of characters
+  * @return dwRealSize the number has been sent
+  */
+int HAL_UART_SerialOut(struct UART_REG *pReg, uint8_t *pdata, uint32_t cnt)
+{
+    int dwRealSize = 0;
+
     while (cnt--) {
-        if (!(pReg->UART_USR & UART_TRANSMIT_FIFO_NOT_FULL)) {
+        if (!(pReg->UART_USR & UART_USR_TX_FIFO_NOT_FULL)) {
             break;
         }
 
@@ -153,15 +287,19 @@ int HAL_UART_WriteByte(eUART_CH uartPort, uint8_t *pdata, uint32_t cnt)
     return dwRealSize;
 }
 
-int HAL_UART_ReadByte(eUART_CH uartPort, uint8_t *pdata, uint32_t cnt)
+/**
+  * @brief  receive many characters
+  * @param  pReg: uart reg base
+  * @param  pdata: characters buffer
+  * @param  cnt: the number of characters
+  * @return dwRealSize the number has been received
+  */
+int HAL_UART_SerialIn(struct UART_REG *pReg, uint8_t *pdata, uint32_t cnt)
 {
-    int dwRealSize;
-    struct UART_REG *pReg = UART_PORT(uartPort);
-
-    dwRealSize = 0;
+    int dwRealSize = 0;
 
     while (cnt--) {
-        if (!(pReg->UART_USR & UART_RECEIVE_FIFO_NOT_EMPTY)) {
+        if (!(pReg->UART_USR & UART_USR_RX_FIFO_NOT_EMPTY)) {
             break;
         }
 
@@ -171,64 +309,131 @@ int HAL_UART_ReadByte(eUART_CH uartPort, uint8_t *pdata, uint32_t cnt)
     return dwRealSize;
 }
 
-HAL_Status HAL_UART_Init(eUART_CH uartPort, eUART_BaudRate baudRate,
-                         eUART_dataLen dataBit, eUART_stopBit stopBit,
-                         eUART_parityEn parity)
+/** @} */
+
+/** @defgroup UART_Exported_Functions_Group4 interrupt Functions
+ @verbatim
+
+ ===============================================================================
+             #### interrupt functions ####
+ ===============================================================================
+ This section provides functions allowing to handler interrupt:
+
+ @endverbatim
+ *  @{
+ */
+/**
+  * @brief  low level irq handler, for msi, busy, rlsi
+  * @param  pReg: uart reg base
+  * @return HAL_OK for reserve
+  */
+HAL_Status HAL_UART_HandleIrq(struct UART_REG *pReg)
 {
-    struct UART_REG *pReg = UART_PORT(uartPort);
+    int iir = 0;
 
-    UART_Rest(uartPort);
+    iir = HAL_UART_GetIrqID(pReg);
 
-    UART_SetFifoEnabledNumb(uartPort, SHADOW_FIFI_ENABLED | RCVR_TRIGGER_ONE |
-                                          TX_TRIGGER_EMPTY);
-
-    UART_SetLcrReg(uartPort, dataBit, parity, stopBit);
-
-    UART_SetBaudRate(uartPort, 24 * 1000 * 1000, baudRate);
-    pReg->UART_SRR = RCVR_FIFO_REST | XMIT_FIFO_RESET;
+    /* Handle the three sub interrupts, so the upper irq handler needn't handle those */
+    switch (iir) {
+    case UART_IIR_MSI:
+        HAL_UART_GetMsr(pReg); /* clear MSI only */
+        break;
+    case UART_IIR_BUSY:
+        HAL_UART_GetUsr(pReg); /* clear BUSY interrupt only */
+        break;
+    case UART_IIR_RLSI:
+        HAL_UART_GetMsr(pReg); /* clear RLSI interrupt only */
+        break;
+    case UART_IIR_NO_INT:
+        break;
+    }
 
     return HAL_OK;
 }
 
-uint32_t HAL_UART_GetIntType(eUART_CH uartPort)
+/** @} */
+
+/** @defgroup UART_Exported_Functions_Group5 Init and Deinit Functions
+ @verbatim
+
+ ===============================================================================
+             #### Init and deinit functions ####
+ ===============================================================================
+ This section provides functions allowing to init and deinit the module:
+
+ @endverbatim
+ *  @{
+ */
+/**
+  * @brief  configure uart baudrate,data bit,stop bit and so on
+  * @param  pReg: uart reg base
+  * @param  baudRate: like UART_BR_115200
+  * @param  dataBit: like UART_DATA_8B
+  * @param  stopBit: like UART_ONE_STOPBIT
+  * @param  parity: like UART_PARITY_DISABLE
+  * @return HAL_OK for reserve
+  */
+HAL_Status HAL_UART_Init(struct UART_REG *pReg, eUART_BaudRate baudRate,
+                         eUART_dataLen dataBit, eUART_stopBit stopBit,
+                         eUART_parityEn parity)
 {
-    uint32_t dwUartIntStaus;
-    uint32_t event;
-    struct UART_REG *pReg = UART_PORT(uartPort);
-
-    dwUartIntStaus = pReg->UART_IIR & UART_IF_MASK;
-    event = 0;
-
-    if (dwUartIntStaus == UART_IF_REC_DATA) {
-        event |= UART_INT_READ_FIFO_NOT_EMPTY;
-    } else if (dwUartIntStaus == UART_IF_THR_EMPTY) {
-        event |= UART_INT_WRITE_FIFO_EMPTY;
-    } else if (dwUartIntStaus == UART_IF_C_TIMEOUT) {
-        event |= UART_INT_READ_FIFO_NOT_EMPTY;
-    } else {
-        if (dwUartIntStaus != 0x01)
-            printf("\n uart err = %x = %x", pReg->UART_LSR, dwUartIntStaus);
-    }
-
-    return event;
+    UART_Reset(pReg);
+    pReg->UART_FCR =
+        UART_FCR_ENABLE_FIFO | UART_FCR_R_TRIG_10 | UART_FCR_T_TRIG_10;
+    UART_SetLcrReg(pReg, dataBit, parity, stopBit);
+    UART_SetBaudRate(pReg, 24000000, baudRate);
+    return HAL_OK;
 }
 
-void HAL_UART_EnableAutoFlowControl(eUART_CH uartPort)
+/**
+  * @brief  disable uart by resetting it
+  * @param  pReg: uart reg base
+  * @return HAL_OK for reserve
+  */
+HAL_Status HAL_UART_DeInit(struct UART_REG *pReg)
 {
-    struct UART_REG *pReg;
-    pReg = UART_PORT(uartPort);
-    pReg->UART_MCR = AUTO_FLOW_ENSABLED | 0X02;
+    UART_Reset(pReg);
+    return HAL_OK;
 }
 
-uint32_t HAL_UART_GetCTSState(eUART_CH uartPort)
+/** @} */
+
+/** @defgroup UART_Exported_Functions_Group6 Suspend and Resume Functions
+ @verbatim
+
+ ===============================================================================
+             #### Suspend and Resume functions ####
+ ===============================================================================
+ This section provides functions allowing to suspend and resume the module:
+
+ @endverbatim
+ *  @{
+ */
+/**
+  * @brief  suspend uart
+  * @param  pReg: uart reg base
+  * @return HAL_OK for reserve
+  */
+HAL_Status HAL_UART_Suspend(struct UART_REG *pReg)
 {
-    struct UART_REG *pReg;
-    uint32_t cts_status;
-
-    pReg = UART_PORT(uartPort);
-    cts_status = pReg->UART_MSR & (1 << 4);
-
-    return cts_status;
+    /* ...to do */
+    return HAL_OK;
 }
+
+/**
+  * @brief  resume uart
+  * @param  pReg: uart reg base
+  * @return HAL_OK for reserve
+  */
+HAL_Status HAL_UART_Resume(struct UART_REG *pReg)
+{
+    /* ...to do */
+    return HAL_OK;
+}
+/** @} */
 
 #endif
+
+/** @} */
+
+/** @} */
