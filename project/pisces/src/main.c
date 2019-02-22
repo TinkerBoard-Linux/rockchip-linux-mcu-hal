0ab46c53261b8abc532aa95fd7c7f682f864b259
Jon Lin <jon.lin@rock-chips.com>
2019-02-22 20:22:04 +0800

    project: pisces: add Unity test
    Change-Id: Ifadc9b890835a2ea81705c334f3f74512d7c1e52
    Signed-off-by: Jon Lin <jon.lin@rock-chips.com>

--- project/pisces/src/main.c
+++ project/pisces/src/main.c
@@ -5,6 +5,7 @@
 
 #include "bsp.h"
 #include "hal_base.h"
+#include "unity_runner.h"
 
 /********************* Private MACRO Definition ******************************/
 
@@ -18,6 +19,37 @@
 
 struct UART_REG *pUart = UART0;
 
+#ifdef __GNUC__
+int _write(int fd, char *ptr, int len)
+{
+    int i = 0;
+
+    /*
+     * write "len" of char from "ptr" to file id "fd"
+     * Return number of char written.
+     *
+    * Only work for STDOUT, STDIN, and STDERR
+     */
+    if (fd > 2)
+    {
+        return -1;
+    }
+
+    while (*ptr && (i < len))
+    {
+        if (*ptr == '\n')
+        {
+            HAL_UART_SerialOut(pUart, "\r", 1);
+        }
+        HAL_UART_SerialOut(pUart, (uint8_t *)ptr, 1);
+
+        i++;
+        ptr++;
+    }
+
+    return i;
+}
+#else
 int fputc(int ch, FILE *f)
 {
     if (ch == '\n') {
@@ -26,8 +58,9 @@ int fputc(int ch, FILE *f)
     HAL_UART_SerialOut(pUart, (uint8_t *)&ch, 1);
 
 }
+#endif
 
-void UART0_IRQHandler(void)
+void UART_IRQHandler(void)
 {
     HAL_UART_HandleIrq(pUart);
 }
@@ -43,12 +76,13 @@ int main(void)
     BSP_Init();
 
     /* UART Init */
-    HAL_NVIC_SetIRQHandler(UART0_IRQn, (NVIC_IRQHandler)&UART0_IRQHandler);
+    HAL_NVIC_SetIRQHandler(UART0_IRQn, (NVIC_IRQHandler)&UART_IRQHandler);
     HAL_NVIC_EnableIRQ(UART0_IRQn);
     HAL_UART_Init(pUart, UART_BR_115200, UART_DATA_8B, UART_ONE_STOPBIT,
                   UART_PARITY_DISABLE);
 
-    printf("print test\n");
+    /* Unity Test  */
+    test_main();
 
     while (1)
         ;
