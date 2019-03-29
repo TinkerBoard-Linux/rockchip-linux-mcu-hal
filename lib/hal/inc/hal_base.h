02c50860e70d2ab7d31e57ac536cc50de99dac1a
Jon Lin <jon.lin@rock-chips.com>
2019-03-29 14:10:04 +0800

    lib: hal: fix hal_base.h multi define in armcc
    Change-Id: I876bf8f4688bfd070ee1778e8b53be62737f2019
    Signed-off-by: Jon Lin <jon.lin@rock-chips.com>

--- lib/hal/inc/hal_base.h
+++ lib/hal/inc/hal_base.h
@@ -30,7 +30,7 @@ HAL_Status HAL_IncTick(void);
 uint32_t HAL_GetTick(void);
 HAL_Status HAL_DelayUs(__IO uint32_t delay);
 HAL_Status HAL_DelayMs(__IO uint32_t delay);
-__weak __irq HAL_Status HAL_SysTick_Handler(void);
+HAL_Status HAL_SysTick_Handler(void);
 HAL_Check HAL_SystickConfig(uint32_t ticksNumb);
 
 #endif
