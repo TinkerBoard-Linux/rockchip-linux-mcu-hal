24c7c47dd3aa128f381a0f4d86fb1a0b1eaaa100
Jon Lin <jon.lin@rock-chips.com>
2019-04-09 15:04:35 +0800

    bsp: RK2108: add bsp code
    Change-Id: I57ffec6a947cd70d8734374eda986dd29943fe20
    Signed-off-by: Jon Lin <jon.lin@rock-chips.com>

---
+++ lib/bsp/RK2108/bsp.c
@@ -0,0 +1,104 @@
+/* SPDX-License-Identifier: BSD-3-Clause */
+/*
+ * Copyright (c) 2019 Rockchip Electronic Co.,Ltd
+ */
+
+#include "bsp.h"
+
+#ifdef HAL_I2S_MODULE_ENABLED
+struct HAL_I2S_DEV g_i2s0Dev =
+{
+    .base = I2S0_BASE,
+    .mclk = MCLK_I2S8CH,
+    .hclk = HCLK_I2S_8CH_GATE,
+    .bclkFs = 64,
+    .rxDmaData =
+    {
+        .addr = (uint32_t)&(I2S0->RXDR),
+        .addrWidth = DMA_SLAVE_BUSWIDTH_4_BYTES,
+        .maxBurst = 8,
+        .dmaReqCh = DMA_REQ_I2S0_RX,
+        .dmac = DMA_BASE,
+    },
+    .txDmaData =
+    {
+        .addr = (uint32_t)&(I2S0->TXDR),
+        .addrWidth = DMA_SLAVE_BUSWIDTH_4_BYTES,
+        .maxBurst = 8,
+        .dmaReqCh = DMA_REQ_I2S0_TX,
+        .dmac = DMA_BASE,
+    },
+};
+
+HAL_Status BSP_I2S_Init(void)
+{
+    /* CLK Init */
+
+    /* IO Init */
+    return HAL_OK;
+}
+
+HAL_Status BSP_I2S_DeInit(void)
+{
+    /* CLK Init */
+
+    /* IO Init */
+    return HAL_OK;
+}
+#endif
+
+#ifdef HAL_PDM_MODULE_ENABLED
+struct HAL_PDM_DEV g_pdm0Dev =
+{
+    .base = PDM0_BASE,
+    .mclk = MCLK_PDM0,
+    .hclk = HCLK_PDM0_GATE,
+    .reset = SRST_M_PDM0,
+    .rxDmaData =
+    {
+        .addr = (uint32_t)&(PDM0->RXFIFO_DATA_REG),
+        .addrWidth = DMA_SLAVE_BUSWIDTH_4_BYTES,
+        .maxBurst = 8,
+        .dmaReqCh = DMA_REQ_PDM0,
+        .dmac = DMA_BASE,
+    },
+};
+
+HAL_Status BSP_PDM_Init(void)
+{
+    /* CLK Init */
+
+    /* IO Init */
+    return HAL_OK;
+}
+
+HAL_Status BSP_PDM_DeInit(void)
+{
+    /* CLK Init */
+
+    /* IO Init */
+    return HAL_OK;
+}
+#endif
+
+void BSP_DeInit(void)
+{
+#ifdef HAL_I2S_MODULE_ENABLED
+    BSP_I2S_Init();
+#endif
+
+#ifdef HAL_PDM_MODULE_ENABLED
+    BSP_PDM_Init();
+#endif
+}
+
+void BSP_Init(void)
+{
+#ifdef HAL_I2S_MODULE_ENABLED
+    BSP_I2S_DeInit();
+#endif
+
+#ifdef HAL_PDM_MODULE_ENABLED
+    BSP_PDM_DeInit();
+#endif
+}
---
+++ lib/bsp/RK2108/bsp.h
@@ -0,0 +1,26 @@
+/* SPDX-License-Identifier: BSD-3-Clause */
+/*
+ * Copyright (c) 2019 Rockchip Electronic Co.,Ltd
+ */
+
+#ifndef __BSP_H__
+#define __BSP_H__
+
+#include "hal_base.h"
+
+/***************************** MACRO Definition ******************************/
+
+/***************************** Structure Definition **************************/
+#ifdef HAL_I2S_MODULE_ENABLED
+extern struct HAL_I2S_DEV g_i2s0Dev;
+#endif
+
+#ifdef HAL_PDM_MODULE_ENABLED
+extern struct HAL_PDM_DEV g_pdm0Dev;
+#endif
+
+/***************************** Function Declare ******************************/
+
+void BSP_Init(void);
+
+#endif
