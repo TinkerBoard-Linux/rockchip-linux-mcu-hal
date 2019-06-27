561cf71650167181044667c8e0b980066d335c36
Jon Lin <jon.lin@rock-chips.com>
2019-06-27 20:43:43 +0800

    project: koala: Add hal test code
    Change-Id: I77474add1769ce82725ebe6804718119e45d87f3
    Signed-off-by: Jon Lin <jon.lin@rock-chips.com>
    Signed-off-by: Andy Yan <andy.yan@rock-chips.com>

---
+++ project/koala/src/hal_conf.h
@@ -0,0 +1,47 @@
+/* SPDX-License-Identifier: BSD-3-Clause */
+/*
+ * Copyright (c) 2019 Rockchip Electronic Co.,Ltd
+ */
+
+#ifndef _HAL_CONF_H_
+#define _HAL_CONF_H_
+
+/* HAL SOC Config */
+#define RKMCU_KOALA
+
+/* HAL Driver Config */
+#define HAL_NVIC_MODULE_ENABLED
+#define HAL_FSPI_MODULE_ENABLED
+#define HAL_SNOR_MODULE_ENABLED
+#define HAL_UART_MODULE_ENABLED
+#define HAL_AUDIO_MODULE_ENABLED
+#define HAL_I2C_MODULE_ENABLED
+#define HAL_I2S_MODULE_ENABLED
+#define HAL_ICACHE_MODULE_ENABLED
+#define HAL_CRU_MODULE_ENABLED
+#define HAL_DCACHE_MODULE_ENABLED
+#define HAL_DMA_MODULE_ENABLED
+#define HAL_GPIO_MODULE_ENABLED
+#define HAL_MBOX_MODULE_ENABLED
+#define HAL_PDM_MODULE_ENABLED
+#define HAL_PMU_MODULE_ENABLED
+#define HAL_PL330_MODULE_ENABLED
+#define HAL_SPI_MODULE_ENABLED
+#define HAL_WDT_MODULE_ENABLED
+
+
+/* HAL_DEG SUB CONFIG */
+#define HAL_DBG_ON 1
+#define HAL_DBG_INFO_ON 1
+#define HAL_DBG_WRN_ON 1
+#define HAL_DBG_ERR_ON 1
+#define USE_FULL_ASSERT 1
+
+/* UNITY CONFIG */
+#define UNITY_INCLUDE_CONFIG_H
+
+#ifdef HAL_SNOR_MODULE_ENABLED
+#define HAL_SNOR_FSPI_HOST
+#endif
+
+#endif
