62961d39e1d2220c15d31efc13e21cb1b959e44a
Tao Huang <huangtao@rock-chips.com>
2019-07-02 20:03:28 +0800

    project: rk2206: comipler add -Wall check
    Change-Id: Iaed61884933c8da0f527cc8b3b841e5f88cb6f44
    Signed-off-by: Tao Huang <huangtao@rock-chips.com>

--- project/rk2206/src/main.c
+++ project/rk2206/src/main.c
@@ -39,7 +39,7 @@ int _write(int fd, char *ptr, int len)
     {
         if (*ptr == '\n')
         {
-            HAL_UART_SerialOut(pUart, "\r", 1);
+            HAL_UART_SerialOut(pUart, (const uint8_t *)"\r", 1);
         }
         HAL_UART_SerialOut(pUart, (uint8_t *)ptr, 1);
 
@@ -92,10 +92,10 @@ int main(void)
 
 int _start(void)
 {
-    main();
+    return main();
 }
 
 int Main(void)
 {
-    main();
+    return main();
 }
