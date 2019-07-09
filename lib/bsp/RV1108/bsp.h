fe48ffb62e9e80a2a462e99e19451951297ab4a2
Tao Huang <huangtao@rock-chips.com>
2019-07-09 17:14:58 +0800

    lib: bsp: rv1108: remove blank line
    Change-Id: I60ca85e9509a5ffdf0da7d10b78649ff14beac94
    Signed-off-by: Tao Huang <huangtao@rock-chips.com>

--- lib/bsp/RV1108/bsp.h
+++ lib/bsp/RV1108/bsp.h
@@ -16,7 +16,6 @@ extern const struct HAL_UART_DEV g_uart0Dev;
 extern const struct HAL_UART_DEV g_uart2Dev;
 #endif
 
-
 /***************************** Function Declare ******************************/
 
 void BSP_Init(void);
