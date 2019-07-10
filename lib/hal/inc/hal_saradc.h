e0b18718d47a211ad65357c5f325899f2b6dea71
Simon Xue <xxm@rock-chips.com>
2019-07-10 08:48:37 +0800

    lib: hal: fix start saradc issue and clear irq in HAL_SARADC_IrqHandler
    The issue could produce sequential two interrupts, fix it.
    
    Change-Id: I55019c390954a0806e69efebe3055a329ae12397
    Signed-off-by: Simon Xue <xxm@rock-chips.com>

--- lib/hal/inc/hal_saradc.h
+++ lib/hal/inc/hal_saradc.h
@@ -35,7 +35,7 @@ HAL_Status HAL_SARADC_Stop(struct SARADC_REG *reg);
 uint32_t HAL_SARADC_GetRaw(struct SARADC_REG *reg);
 HAL_Check HAL_SARADC_PollIsEOC(struct SARADC_REG *reg);
 void HAL_SARADC_IrqHandler(struct SARADC_REG *reg);
-
+void HAL_SARADC_ClearIrq(struct SARADC_REG *reg);
 #endif
 
 /** @} */
--- lib/hal/src/hal_saradc.c
+++ lib/hal/src/hal_saradc.c
@@ -57,14 +57,19 @@
  */
 HAL_Status HAL_SARADC_Start(struct SARADC_REG *reg, eSARADC_mode mode, uint32_t chn)
 {
+    uint32_t value;
+
     HAL_ASSERT(IS_SARADC_INSTANCE(reg));
     reg->DLY_PU_SOC = 0x8;
-    reg->CTRL = chn & SARADC_CTRL_ADC_INPUT_SRC_SEL_MASK;
+
+    value = chn & SARADC_CTRL_ADC_INPUT_SRC_SEL_MASK;
 
     if (mode == SARADC_INT_MOD)
-        reg->CTRL |= SARADC_CTRL_INT_EN_MASK;
+        value |= SARADC_CTRL_INT_EN_MASK;
 
-    reg->CTRL |= SARADC_CTRL_ADC_POWER_CTRL_MASK;
+    value |= SARADC_CTRL_ADC_POWER_CTRL_MASK;
+
+    reg->CTRL = value;
 
     return HAL_OK;
 }
@@ -77,10 +82,27 @@ HAL_Status HAL_SARADC_Start(struct SARADC_REG *reg, eSARADC_mode mode, uint32_t 
 HAL_Status HAL_SARADC_Stop(struct SARADC_REG *reg)
 {
     HAL_ASSERT(IS_SARADC_INSTANCE(reg));
+
     /* Clear irq & power down adc */
     reg->CTRL = 0x0;
 
     return HAL_OK;
+}
+
+/**
+ * @brief  clear irq
+ * @param  reg: saradc base address
+ */
+void HAL_SARADC_ClearIrq(struct SARADC_REG *reg)
+{
+    uint32_t tmp;
+
+    HAL_ASSERT(IS_SARADC_INSTANCE(reg));
+
+    /* Clear irq */
+    tmp = reg->CTRL;
+    tmp &= ~SARADC_CTRL_INT_STATUS_MASK;
+    reg->CTRL = tmp;
 }
 
 /**
@@ -113,7 +135,14 @@ HAL_Check HAL_SARADC_PollIsEOC(struct SARADC_REG *reg)
  */
 void HAL_SARADC_IrqHandler(struct SARADC_REG *reg)
 {
-    /* nothing to do right now, reserved */
+    uint32_t tmp;
+
+    HAL_ASSERT(IS_SARADC_INSTANCE(reg));
+
+    /* Clear irq */
+    tmp = reg->CTRL;
+    tmp &= ~SARADC_CTRL_INT_STATUS_MASK;
+    reg->CTRL = tmp;
 }
 
 /** @} */
