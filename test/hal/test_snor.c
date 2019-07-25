befa4ad160dfff03efa4e7dd4628adb38a455250
Jon Lin <jon.lin@rock-chips.com>
2019-07-25 19:59:08 +0800

    test: snor: add read write rate dump
    Change-Id: I404acbbe54e70c4cdad2bd6aff7bb4dca8244a5d
    Signed-off-by: Jon Lin <jon.lin@rock-chips.com>

--- test/hal/test_snor.c
+++ test/hal/test_snor.c
@@ -24,7 +24,7 @@ struct cmdSet {
 
 HAL_Status SNOR_SINGLE_TEST(void)
 {
-    uint32_t i, testLba = 0x80;
+    uint32_t ret, i, testLba = 0x80;
 
     pwrite32 = (uint32_t *)pwrite;
     pread32 = (uint32_t *)pread;
@@ -35,10 +35,17 @@ HAL_Status SNOR_SINGLE_TEST(void)
     pwrite32[0] = testLba;
 
     HAL_SNOR_Erase(nor, testLba << 9, 0);
-    HAL_SNOR_ProgData(nor, testLba << 9, pwrite32, 0x100);
+    ret = HAL_GetTick();
+    HAL_SNOR_ProgData(nor, testLba << 9, pwrite32, 0x1000);
+    ret = HAL_GetTick() - ret;
+    HAL_DBG("write %lu KB/s\n", 4 * 1000 / ret);
     memset(pread, 0, 256);
-    HAL_SNOR_ReadData(nor, testLba << 9, pread32, 0x100);
-    for (int32_t i = 0; i < 256; i++) {
+    ret = HAL_GetTick();
+    for (int i = 0; i < 1024; i++)
+        HAL_SNOR_ReadData(nor, testLba << 9, pread32, 0x1000);
+    ret = HAL_GetTick() - ret;
+    HAL_DBG("read %lu MB/s\n", 4 * 1000 / ret);
+    for (int32_t i = 0; i < (0x1000 / 4); i++) {
         if (pwrite[i] != pread[i]) {
             HAL_DBG_HEX("w", pwrite, 4, 64);
             HAL_DBG_HEX("r", pread, 4, 64);
@@ -54,20 +61,19 @@ HAL_Status SNOR_SINGLE_TEST(void)
 
 static HAL_Status SNOR_TEST(uint32_t testEndLBA)
 {
-    uint32_t ret, i, j;
+    int32_t ret, i, j;
     uint32_t testLBA = 0;
-    uint32_t testSecCount = 1;
+    int32_t testSecCount = 1;
 
     pwrite32 = (uint32_t *)pwrite;
     pread32 = (uint32_t *)pread;
-    for (i = 0; i < (maxest_sector * nor->sectorSize / 4); i++)
+    for (i = 0; i < (maxest_sector * (int32_t)nor->sectorSize / 4); i++)
         pwrite32[i] = i;
 
     HAL_DBG("---------Test SNOR STRESS TEST---------\n");
     HAL_DBG("---------Test ftl write---------\n");
     testSecCount = 1;
     HAL_DBG("testEndLBA = %lx\n", testEndLBA);
-    HAL_DBG("testLBA = %lx\n", testLBA);
     for (testLBA = 0; (testLBA + testSecCount) < testEndLBA;) {
         pwrite32[0] = testLBA;
         ret = HAL_SNOR_OverWrite(nor, testLBA, testSecCount, pwrite32);
@@ -77,7 +83,7 @@ static HAL_Status SNOR_TEST(uint32_t testEndLBA)
         ret = HAL_SNOR_Read(nor, testLBA, testSecCount, pread32);
         if (ret != testSecCount)
             return ret;
-        for (j = 0; j < testSecCount * nor->sectorSize / 4; j++) {
+        for (j = 0; j < testSecCount * (int32_t)nor->sectorSize / 4; j++) {
             if (pwrite32[j] != pread32[j]) {
                 HAL_DBG_HEX("w:", pwrite32, 4, 16);
                 HAL_DBG_HEX("r:", pread32, 4, 16);
@@ -103,10 +109,10 @@ static HAL_Status SNOR_TEST(uint32_t testEndLBA)
         ret = HAL_SNOR_Read(nor, testLBA, testSecCount, pread32);
         if (ret != testSecCount)
             return ret;
-        for (j = 0; j < testSecCount * nor->sectorSize / 4; j++) {
+        for (j = 0; j < testSecCount * (int32_t)nor->sectorSize / 4; j++) {
             if (pwrite32[j] != pread32[j]) {
-                HAL_DBG_HEX("w:", pwrite32, 4, testSecCount * nor->sectorSize / 4);
-                HAL_DBG_HEX("r:", pread32, 4, testSecCount * nor->sectorSize / 4);
+                HAL_DBG_HEX("w:", pwrite32, 4, testSecCount * (int32_t)nor->sectorSize / 4);
+                HAL_DBG_HEX("r:", pread32, 4, testSecCount * (int32_t)nor->sectorSize / 4);
                 HAL_DBG(
                     "recheck not match:row=%lx, num=%lx, write=%lx, read=%lx\n",
                     testLBA, j, pwrite32[j], pread32[j]);
@@ -333,9 +339,9 @@ TEST_GROUP_RUNNER(HAL_SNOR){
     HAL_DBG("pwrite %p pread %p\n", pwrite, pread);
     TEST_ASSERT_NOT_NULL(pread);
     TEST_ASSERT_NOT_NULL(pwrite);
-    spi = (struct SNOR_HOST *)malloc(sizeof(struct SNOR_HOST));
+    spi = (struct SNOR_HOST *)calloc(1, sizeof(struct SNOR_HOST));
     TEST_ASSERT_NOT_NULL(spi);
-    nor = (struct SPI_NOR *)malloc(sizeof(struct SPI_NOR));
+    nor = (struct SPI_NOR *)calloc(1, sizeof(struct SPI_NOR));
     TEST_ASSERT_NOT_NULL(nor);
     nor->spi = spi;
 
@@ -343,7 +349,7 @@ TEST_GROUP_RUNNER(HAL_SNOR){
     uint32_t ret;
     static struct HAL_FSPI_HOST *fspiHost;
 
-    fspiHost = (struct HAL_FSPI_HOST *)malloc(sizeof(struct HAL_FSPI_HOST));
+    fspiHost = (struct HAL_FSPI_HOST *)calloc(1, sizeof(struct HAL_FSPI_HOST));
     TEST_ASSERT_NOT_NULL(fspiHost);
 
     fspiHost->instance = FSPI0;
@@ -362,7 +368,7 @@ TEST_GROUP_RUNNER(HAL_SNOR){
     uint32_t ret;
     static struct SPI_HANDLE *spiHost;
 
-    spiHost = (struct SPI_HANDLE *)malloc(sizeof(struct SPI_HANDLE));
+    spiHost = (struct SPI_HANDLE *)calloc(1, sizeof(struct SPI_HANDLE));
     TEST_ASSERT_NOT_NULL(spiHost);
 
     spiHost->config.clkPolarity = CR0_POLARITY_HIGH;
