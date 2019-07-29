b9ecebd1dfb7cd99ef502f7f2a6d271783d1b1c1
Jon Lin <jon.lin@rock-chips.com>
2019-07-29 20:05:28 +0800

    devices: pisces: adjust __NVIC_PRIO_BITS to 3
    Change-Id: I238de51577ae690ce05b6b39be6f904872779156
    Signed-off-by: Jon Lin <jon.lin@rock-chips.com>

--- lib/CMSIS/Device/Pisces/Include/soc.h
+++ lib/CMSIS/Device/Pisces/Include/soc.h
@@ -88,7 +88,7 @@ typedef enum
 #define __CM4_REV                 0x0001U  /* Core revision r0p1                    */
 #define __MPU_PRESENT             1U       /* Pisces provides an MPU                */
 #define __VTOR_PRESENT            1U       /* VTOR present */
-#define __NVIC_PRIO_BITS          4U       /* Pisces uses 4 Bits for the Priority Levels */
+#define __NVIC_PRIO_BITS          3U       /* Pisces uses 3 Bits for the Priority Levels */
 #define __Vendor_SysTickConfig    0U       /* Set to 1 if different SysTick Config is used  */
 #define __FPU_PRESENT             1U       /* FPU present                                   */
 
