7f1127f0255ecf8ec274c38ac12867801955e872
Jon Lin <jon.lin@rock-chips.com>
2019-07-30 11:49:26 +0800

    project: rk2108: add CLK init
    Change-Id: Ia89d1322558f107ce23e56646ff43af238d7ef7d
    Signed-off-by: Jon Lin <jon.lin@rock-chips.com>

--- project/rk2108/src/main.c
+++ project/rk2108/src/main.c
@@ -17,7 +17,7 @@
 
 /********************* Public Function Definition ****************************/
 
-static struct UART_REG *pUart = UART2;
+static struct UART_REG *pUart = UART0;
 
 #ifdef __GNUC__
 int _write(int fd, char *ptr, int len)
@@ -62,6 +62,57 @@ void UART_IRQHandler(void)
     HAL_UART_HandleIrq(pUart);
 }
 
+#define DUMP_CLK(NAME, ID, RATE) \
+    { .name = NAME, .clkId = ID, .initRate = RATE, }
+
+struct CLK_DUMP {
+    const char *name;
+    uint32_t clkId;
+    uint32_t initRate;
+};
+
+static const struct CLK_DUMP s_clkInits[] =
+{
+    DUMP_CLK("SCLK_SHRM", SCLK_SHRM, 10 * MHZ),
+    DUMP_CLK("PCLK_SHRM", PCLK_SHRM, 10 * MHZ),
+    DUMP_CLK("PCLK_ALIVE", PCLK_ALIVE, 10 * MHZ),
+    DUMP_CLK("HCLK_ALIVE", HCLK_ALIVE, 10 * MHZ),
+    DUMP_CLK("HCLK_M4", HCLK_M4, 10 * MHZ),
+    DUMP_CLK("ACLK_LOGIC", ACLK_LOGIC, 10 * MHZ),
+    DUMP_CLK("HCLK_LOGIC", HCLK_LOGIC, 10 * MHZ),
+    DUMP_CLK("PCLK_LOGIC", PCLK_LOGIC, 10 * MHZ),
+    DUMP_CLK("SCLK_SFC_SRC", SCLK_SFC_SRC, 5 * MHZ),
+    DUMP_CLK("CLK_SPI1", CLK_SPI1, 5 * MHZ),
+    DUMP_CLK("CLK_SPI2", CLK_SPI2, 5 * MHZ),
+    DUMP_CLK("PLL_GPLL", PLL_GPLL, 1188 * MHZ),
+    DUMP_CLK("PLL_CPLL", PLL_CPLL, 1000 * MHZ),
+    DUMP_CLK("SCLK_SFC_SRC", SCLK_SFC_SRC, 100 * MHZ),
+    DUMP_CLK("HCLK_M4", HCLK_M4, 300 * MHZ),
+    DUMP_CLK("ACLK_DSP", ACLK_DSP, 400 * MHZ),
+    DUMP_CLK("ACLK_LOGIC", ACLK_LOGIC, 300 * MHZ),
+    DUMP_CLK("HCLK_LOGIC", HCLK_LOGIC, 150 * MHZ),
+    DUMP_CLK("PCLK_LOGIC", PCLK_LOGIC, 150 * MHZ),
+    DUMP_CLK("SCLK_SHRM", SCLK_SHRM, 300 * MHZ),
+    DUMP_CLK("PCLK_SHRM", PCLK_SHRM, 100 * MHZ),
+    DUMP_CLK("PCLK_ALIVE", PCLK_ALIVE, 100 * MHZ),
+    DUMP_CLK("HCLK_ALIVE", HCLK_ALIVE, 100 * MHZ),
+    DUMP_CLK("CLK_SPI1", CLK_SPI1, 50 * MHZ),
+    DUMP_CLK("CLK_SPI2", CLK_SPI2, 50 * MHZ),
+};
+
+void ClkInit(const struct CLK_DUMP *clkInits, int clkInitNum, bool clkDump)
+{
+    int i;
+
+    for (i = 0; i < clkInitNum; i++) {
+        if (clkInits[i].initRate) {
+            HAL_CRU_ClkSetFreq(clkInits[i].clkId, clkInits[i].initRate);
+        }
+        if (clkDump)
+            HAL_DBG("%s: %s = %ld\n", __func__, clkInits[i].name, HAL_CRU_ClkGetFreq(clkInits[i].clkId));
+    }
+}
+
 int main(void)
 {
     struct HAL_UART_CONFIG hal_uart_config = {
@@ -77,6 +128,8 @@ int main(void)
 
     /* BSP Init */
     BSP_Init();
+
+    ClkInit(s_clkInits, HAL_ARRAY_SIZE(s_clkInits), 0);
 
     /* UART Init */
     HAL_NVIC_SetIRQHandler(UART0_IRQn, UART_IRQHandler);
