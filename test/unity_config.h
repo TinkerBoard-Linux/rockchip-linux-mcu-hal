405fac322527d24ddac5d871ff0d9a3fa384eb8b
Jon Lin <jon.lin@rock-chips.com>
2019-07-30 11:19:54 +0800

    test: unity: add limitation for test
    Change-Id: I1e55b00622b59523ad3a6554aee4a1b9679c8708
    Signed-off-by: Jon Lin <jon.lin@rock-chips.com>

--- test/unity_config.h
+++ test/unity_config.h
@@ -6,14 +6,17 @@
 #ifndef UNITY_CONFIG_H
 #define UNITY_CONFIG_H
 
+/************** Unity Config *********************/
 #define UNITY_INT_WIDTH 32
-
 #define UNITY_OUTPUT_CHAR(a) printf
 
+/************** HAL Module config ****************/
 //#define UNITY_HAL_COREMARK
 //#define UNITY_HAL_DATA_ACCESS
 #define UNITY_HAL_THROUGHPUT
+//#define UNITY_HAL_PSRAM
 
+/************** HAL Macro Define *****************/
 #if defined(RKMCU_RK2108)
 #define XIP_MEM_BASE 0x60000000
 #elif defined(RKMCU_RK2206)
