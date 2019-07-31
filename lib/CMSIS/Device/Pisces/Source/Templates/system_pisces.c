5c21af8b6ff253b06c25ed9e5da50b4705d8c546
Cliff Chen <cliff.chen@rock-chips.com>
2019-07-31 10:19:27 +0800

    device: rk2108 & pisces: Enable prefetch for dcache & icache
    Change-Id: I0369db4e40d4444f98bc09c1c70dc15f4fb712b1
    Signed-off-by: Cliff Chen <cliff.chen@rock-chips.com>

--- lib/CMSIS/Device/Pisces/Source/Templates/system_pisces.c
+++ lib/CMSIS/Device/Pisces/Source/Templates/system_pisces.c
@@ -47,9 +47,9 @@ void SystemInit(void)
 #endif
 
     /* config icache: mpu disable, stb disable, write through, hot buffer enable */
-    ICACHE->CACHE_CTRL |=
-        (ICACHE_CACHE_CTRL_CACHE_EN_MASK | ICACHE_CACHE_CTRL_CACHE_WT_EN_MASK |
-         ICACHE_CACHE_CTRL_CACHE_MPU_MODE_MASK);
+    ICACHE->CACHE_CTRL |= ICACHE_CACHE_CTRL_CACHE_PF_EN_MASK |
+                          (ICACHE_CACHE_CTRL_CACHE_EN_MASK | ICACHE_CACHE_CTRL_CACHE_WT_EN_MASK |
+                           ICACHE_CACHE_CTRL_CACHE_MPU_MODE_MASK);
     ICACHE->CACHE_CTRL &= (~ICACHE_CACHE_CTRL_CACHE_STB_EN_MASK);
 
     do {
@@ -60,7 +60,7 @@ void SystemInit(void)
     ICACHE->CACHE_CTRL &= ~ICACHE_CACHE_CTRL_CACHE_BYPASS_MASK;
 
     /* stb enable, stb_entry=7, stb_timeout enable, write back */
-    DCACHE->CACHE_CTRL |= DCACHE_CACHE_CTRL_CACHE_EN_MASK |
+    DCACHE->CACHE_CTRL |= DCACHE_CACHE_CTRL_CACHE_EN_MASK | DCACHE_CACHE_CTRL_CACHE_PF_EN_MASK |
                           (7U << DCACHE_CACHE_CTRL_CACHE_ENTRY_THRESH_SHIFT) |
                           DCACHE_CACHE_CTRL_STB_TIMEOUT_EN_MASK |
                           DCACHE_CACHE_CTRL_CACHE_MPU_MODE_MASK;
