56f88833e1a234b483cbecf986cb310ce75514af
Elaine Zhang <zhangqing@rock-chips.com>
2019-08-01 09:49:02 +0800

    lib: hal: cru: rk2108: Adjust PLL frequency table
    Remove unnecessary frequency points,
    modify PLL VCO to reduce PLL power.
    
    Change-Id: I2ae471841e08f8665239b75c5f8807efe23bc3ea
    Signed-off-by: Elaine Zhang <zhangqing@rock-chips.com>

--- lib/hal/src/cru/hal_cru_rk2108.c
+++ lib/hal/src/cru/hal_cru_rk2108.c
@@ -22,11 +22,8 @@
 
 static struct PLL_CONFIG PLL_TABLE[] = {
     /* _mhz, _refDiv, _fbDiv, _postdDv1, _postDiv2, _dsmpd, _frac */
-    RK_PLL_RATE(1200000000, 1, 50, 1, 1, 1, 0),
-    RK_PLL_RATE(1188000000, 1, 99, 2, 1, 1, 0),
-    RK_PLL_RATE(1008000000, 1, 84, 2, 1, 1, 0),
-    RK_PLL_RATE(816000000, 1, 68, 2, 1, 1, 0),
-    RK_PLL_RATE(600000000, 1, 75, 3, 1, 1, 0),
+    RK_PLL_RATE(1188000000, 2, 99, 1, 1, 1, 0),
+    RK_PLL_RATE(600000000, 3, 75, 1, 1, 1, 0),
     { /* sentinel */ },
 };
 
