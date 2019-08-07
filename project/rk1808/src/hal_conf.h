47d300f9f68a06616a68cf09ba00c6d7540477f8
Tao Huang <huangtao@rock-chips.com>
2019-08-07 17:15:20 +0800

    project: rk1808: add compile support
    Change-Id: If2df2418e5e14b7a8a339a20f8e52a92b9e0ca40
    Signed-off-by: Tao Huang <huangtao@rock-chips.com>

---
+++ project/rk1808/src/hal_conf.h
@@ -0,0 +1,43 @@
+/* SPDX-License-Identifier: BSD-3-Clause */
+/*
+ * Copyright (c) 2019 Fuzhou Rockchip Electronics Co., Ltd
+ */
+
+#ifndef _HAL_CONF_H_
+#define _HAL_CONF_H_
+
+#define SYS_TIMER TIMER5 /* System timer designation (RK TIMER) */
+
+/* DEG LEVEL */
+#define HAL_DBG_ON      1       /* the master switch for hal debug log, comment out it will disable all of hal log */
+#define HAL_DBG_INFO_ON 1       /* enable hal log with info level */
+#define HAL_DBG_WRN_ON  1       /* enable hal log with warning level */
+#define HAL_DBG_ERR_ON  1       /* enable hal log with error level */
+#define USE_FULL_ASSERT 1
+
+#define HAL_CRU_MODULE_ENABLED
+#define HAL_DMA_MODULE_ENABLED
+#define HAL_PL330_MODULE_ENABLED
+#define HAL_I2C_MODULE_ENABLED
+//#define HAL_I2S_MODULE_ENABLED
+#define HAL_I2STDM_MODULE_ENABLED
+#define HAL_PDM_MODULE_ENABLED
+#define HAL_GPIO_MODULE_ENABLED
+#define HAL_PINCTRL_MODULE_ENABLED
+#define HAL_PWR_I2C8_MODULE_ENABLED
+#define HAL_PWR_MODULE_ENABLED
+//#define HAL_PMU_MODULE_ENABLED
+#define HAL_SFC_MODULE_ENABLED
+#define HAL_SNOR_MODULE_ENABLED
+#define HAL_SNOR_SFC_HOST
+//#define HAL_SPI_MODULE_ENABLED
+#define HAL_UART_MODULE_ENABLED
+//#define HAL_VAD_MODULE_ENABLED
+#define HAL_SARADC_MODULE_ENABLED
+#define HAL_SDIO_MODULE_ENABLED
+#define HAL_SDIO_MODULE_ENABLED
+#define HAL_ICACHE_MODULE_ENABLED
+#define HAL_DCACHE_MODULE_ENABLED
+#define HAL_TIMER_MODULE_ENABLED
+
+#endif /* _HAL_CONF_H_ */
