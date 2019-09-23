1c3f6989301907326f5d8f978d27d3178bc441f3
Liao Huaping <huaping.liao@rock-chips.com>
2019-09-23 17:15:21 +0800

    common: lib: dsp: Move hal_dsp.c to chip hal_dsp_*.c
    Change-Id: I43f0d1591f2faf302d2de3274f621655f5df4ae7
    Signed-off-by: Liao Huaping <huaping.liao@rock-chips.com>

--- lib/hal/inc/hal_dsp.h
+++ lib/hal/inc/hal_dsp.h
@@ -32,6 +32,7 @@
 /**
  * enum DSP_TcmSel - dsp tcm select.
  */
+#if (defined(RKMCU_RK2108) || defined(RKMCU_PISCES))
 typedef enum {
     DSP_DTCM0 = DSP_TCM_SEL(0),    /* 0x30200000 ~ 0x30210000 */
     DSP_DTCM1 = DSP_TCM_SEL(1),    /* 0x30210000 ~ 0x30220000 */
@@ -46,7 +47,31 @@ typedef enum {
 
 #define DSP_DTCM (DSP_DTCM0 | DSP_DTCM1 | DSP_DTCM2 | DSP_DTCM3 | \
                   DSP_DTCM4 | DSP_DTCM5 | DSP_DTCM6 | DSP_DTCM7)
-#define DSP_TCM  (DSP_DTCM | DSP_ITCM)
+#else
+typedef enum {
+    DSP_ITCM0  = DSP_TCM_SEL(0),    /* 0x20400000 ~ 0x20404000 */
+    DSP_ITCM1  = DSP_TCM_SEL(1),    /* 0x20404000 ~ 0x20408000 */
+    DSP_DTCM0  = DSP_TCM_SEL(4),    /* 0x20600000 ~ 0x20604000 */
+    DSP_DTCM1  = DSP_TCM_SEL(5),    /* 0x20604000 ~ 0x20608000 */
+    DSP_DTCM2  = DSP_TCM_SEL(6),    /* 0x20608000 ~ 0x2060c000 */
+    DSP_DTCM3  = DSP_TCM_SEL(7),    /* 0x2060c000 ~ 0x20610000 */
+    DSP_DTCM4  = DSP_TCM_SEL(8),    /* 0x20610000 ~ 0x20614000 */
+    DSP_DTCM5  = DSP_TCM_SEL(9),    /* 0x20614000 ~ 0x20618000 */
+    DSP_DTCM6  = DSP_TCM_SEL(10),   /* 0x20618000 ~ 0x2061c000 */
+    DSP_DTCM7  = DSP_TCM_SEL(11),   /* 0x2061c000 ~ 0x20620000 */
+    DSP_DTCM8  = DSP_TCM_SEL(12),   /* 0x20620000 ~ 0x20624000 */
+    DSP_DTCM9  = DSP_TCM_SEL(13),   /* 0x20624000 ~ 0x20628000 */
+    DSP_DTCM10 = DSP_TCM_SEL(14),   /* 0x20628000 ~ 0x2062c000 */
+    DSP_DTCM11 = DSP_TCM_SEL(15),   /* 0x2062c000 ~ 0x20630000 */
+} eDSP_tcmSel;
+
+#define DSP_DTCM (DSP_DTCM0 | DSP_DTCM1 | DSP_DTCM2 | DSP_DTCM3 |  \
+                  DSP_DTCM4 | DSP_DTCM5 | DSP_DTCM6 | DSP_DTCM7  | \
+                  DSP_DTCM8 | DSP_DTCM9 | DSP_DTCM10 | DSP_DTCM11)
+#define DSP_ITCM (DSP_ITCM0 | DSP_ITCM1)
+#endif
+
+#define DSP_TCM (DSP_DTCM | DSP_ITCM)
 
 /***************************** Structure Definition **************************/
 
@@ -100,15 +125,76 @@ struct DSP_DEV {
 /***************************** Function Declare ******************************/
 
 /** should hold lock to call these functions */
+
+/**
+ * @brief  Get dsp power state.
+ * @return eDSP_powerSt
+ */
 eDSP_powerSt HAL_DSP_GetPowerSt(void);
+
+/**
+ * @brief  Wait dsp power status be set.
+ * @param  status: wait status be set to this value.
+ * @param  timeout: timeout valuse, unit is ms.
+ * @return HAL_Status
+ */
 HAL_Status HAL_DSP_WaitForPowerSt(eDSP_powerSt status, uint32_t timeout);
+
+/**
+ * @brief  Soft interrupt wakeup dsp.
+ * @return HAL_Status
+ */
 HAL_Status HAL_DSP_SoftWakeup(void);
+
+/**
+ * @brief  Set tcm work mode for retention or power down.
+ * @param  tcmSel: tcm "|" combination, such as DSP_DTCM0 | DSP_ITCM.
+ * @param  mode: work mode in eDSP_tCMMODE.
+ * @return HAL_Status
+ */
 HAL_Status HAL_DSP_SetTcmMode(uint32_t tcmSel, eDSP_tcmMode mode);
+
+/**
+ * @brief  Init dsp and deassert reset.
+ * @param  dsp: the handle of dsp.
+ * @return HAL_Status
+ */
 HAL_Status HAL_DSP_Init(struct DSP_DEV *dsp);
+
+/**
+ * @brief  DeInit dsp.
+ * @param  dsp: the handle of dsp.
+ * @return HAL_Status
+ */
 HAL_Status HAL_DSP_DeInit(struct DSP_DEV *dsp);
+
+/**
+ * @brief  Enable dsp.
+ * @param  dsp: the handle of dsp.
+ * @param  altAddr: alternate reset vector address.
+ * @return HAL_Status
+ */
 HAL_Status HAL_DSP_Enable(struct DSP_DEV *dsp, uint32_t altAddr);
+
+/**
+ * @brief  Disable dsp.
+ * @param  dsp: the handle of dsp.
+ * @return HAL_Status
+ */
 HAL_Status HAL_DSP_Disable(struct DSP_DEV *dsp);
+
+/**
+ * @brief  Start dsp.
+ * @param  dsp: the handle of dsp.
+ * @return HAL_Status
+ */
 HAL_Status HAL_DSP_Start(struct DSP_DEV *dsp);
+
+/**
+ * @brief  Stop dsp.
+ * @param  dsp: the handle of dsp.
+ * @return HAL_Status
+ */
 HAL_Status HAL_DSP_Stop(struct DSP_DEV *dsp);
 
 #endif
--- lib/hal/src/dsp/hal_dsp_rk2108.c
+++ lib/hal/src/dsp/hal_dsp_rk2108.c
@@ -20,16 +20,18 @@
 
 #include "hal_base.h"
 
-#ifdef HAL_DSP_MODULE_ENABLED
+#if (defined(RKMCU_PISCES) ||  defined(RKMCU_RK2108)) && defined(HAL_DSP_MODULE_ENABLED)
 
 /********************* Private MACRO Definition ******************************/
-
-#define TCM_SEL_ALIGN (64 * 1024)
-#define TCM_MAX_NUM   (3)
 
 /********************* Private Structure Definition **************************/
 
 /********************* Private Variable Definition ***************************/
+
+static int DSP_Ioctl(void *priv, int cmd, void *arg);
+static const struct DSP_OPS dspOps = {
+    .ioctl = DSP_Ioctl,
+};
 
 /********************* Private Function Definition ***************************/
 
@@ -39,7 +41,6 @@
  * @param  mode: work mode in eDSP_tcmMode.
  * @return HAL_Status
  */
-#if (defined(RKMCU_RK2108) || defined(RKMCU_PISCES))
 static HAL_Status DSP_SetTcmMode(uint32_t tcmSel, eDSP_tcmMode mode)
 {
     uint32_t mask0 = 0;
@@ -120,50 +121,12 @@ static HAL_Status DSP_SetTcmMode(uint32_t tcmSel, eDSP_tcmMode mode)
 
     return HAL_OK;
 }
-#endif
 
 static int DSP_Ioctl(void *priv, int cmd, void *arg)
 {
     HAL_Status ret = HAL_OK;
 
     switch (cmd) {
-#if defined(RKMCU_RK2206)
-    case DSP_IOCTL_SET_ITCM_SIZE:
-    {
-        uint32_t setBit = 0;
-        if ((uint32_t)arg > 0)
-            setBit = GRF_SOC_CON0_DSP_TCM_SEL0_MASK;
-        WRITE_REG_MASK_WE(GRF->SOC_CON0,
-                          GRF_SOC_CON0_DSP_TCM_SEL0_MASK, setBit);
-        break;
-    }
-    case DSP_IOCTL_SET_DTCM_SIZE:
-    {
-        uint32_t sel = 0x7;
-        uint32_t setBit = 0;
-        uint32_t mask = 0;
-        uint32_t num = ((uint32_t)arg + TCM_SEL_ALIGN) / (TCM_SEL_ALIGN + 1);
-
-        if (num > TCM_MAX_NUM)
-            num = TCM_MAX_NUM;
-        num = TCM_MAX_NUM - num;
-        sel = sel >> num;                   /* Get sel */
-
-        if (sel & 0x1)
-            setBit |= GRF_SOC_CON0_DSP_TCM_SEL1_MASK;
-        if (sel & 0x2)
-            setBit |= GRF_SOC_CON0_DSP_TCM_SEL2_MASK;
-        if (sel & 0x4)
-            setBit |= GRF_SOC_CON0_DSP_TCM_SEL3_MASK;
-
-        mask |= GRF_SOC_CON0_DSP_TCM_SEL1_MASK |
-                GRF_SOC_CON0_DSP_TCM_SEL2_MASK |
-                GRF_SOC_CON0_DSP_TCM_SEL3_MASK;
-        WRITE_REG_MASK_WE(GRF->SOC_CON0, mask, setBit);
-        break;
-    }
-#endif
-#if (defined(RKMCU_RK2108) || defined(RKMCU_PISCES) || defined(RKMCU_RK2206))
     case DSP_IOCTL_SET_MEM_GATING:
     {
         uint32_t mask = 0;
@@ -180,26 +143,6 @@ static int DSP_Ioctl(void *priv, int cmd, void *arg)
             WRITE_REG_MASK_WE(GRF->DSP_CON2, mask, 0);
         break;
     }
-#elif defined(RKMCU_KOALA)
-    case DSP_IOCTL_SET_MEM_GATING:
-    {
-        uint32_t mask = 0;
-        mask |= GRF_DSP_CON2_ICACHE_MEM_AUTO_GATING_DISABLE_MASK |
-                GRF_DSP_CON2_ITAG_MEM_AUTO_GATING_DISABLE_MASK |
-                GRF_DSP_CON2_DCACHE_MEM_AUTO_GATING_DISABLE_MASK |
-                GRF_DSP_CON2_DTAG_MEM_AUTO_GATING_DISABLE_MASK |
-                GRF_DSP_CON2_PREFETCH_RAM_AUTO_GATING_DISABLE_MASK |
-                GRF_DSP_CON2_DTCM_MEM_AUTO_GATING_DISABLE_MASK |
-                GRF_DSP_CON2_ITCM_MEM_AUTO_GATING_DISABLE_MASK;
-        if ((uint32_t)arg) {
-            // set value 0 is enable
-            WRITE_REG_MASK_WE(GRF->DSP_CON2, mask, 0);
-        } else {
-            WRITE_REG_MASK_WE(GRF->DSP_CON2, mask, mask);
-        }
-        break;
-    }
-#endif
     default:
         break;
     }
@@ -207,25 +150,15 @@ static int DSP_Ioctl(void *priv, int cmd, void *arg)
     return ret;
 }
 
-static const struct DSP_OPS dspOps = {
-    .ioctl = DSP_Ioctl,
-};
+/********************* Public Function Definition ***************************/
 
 /** @defgroup DSP_Exported_Functions_Group4 Init and DeInit Functions
 
  This section provides functions allowing to init and deinit the module:
 
- ...to do or delete this row
-
  *  @{
  */
 
-#if (defined(RKMCU_RK2108) || defined(RKMCU_PISCES))
-
-/**
- * @brief  Get dsp power state.
- * @return eDSP_powerSt
- */
 eDSP_powerSt HAL_DSP_GetPowerSt(void)
 {
     uint32_t val = READ_REG(PMU->POWER_ST);
@@ -235,12 +168,6 @@ eDSP_powerSt HAL_DSP_GetPowerSt(void)
     return val;
 }
 
-/**
- * @brief  Wait dsp power status be set.
- * @param  st: wait status be set to this value.
- * @param  timeout: timeout valuse, unit is ms.
- * @return HAL_Status
- */
 HAL_Status HAL_DSP_WaitForPowerSt(eDSP_powerSt status, uint32_t timeout)
 {
     uint32_t start;
@@ -254,10 +181,6 @@ HAL_Status HAL_DSP_WaitForPowerSt(eDSP_powerSt status, uint32_t timeout)
     return HAL_OK;
 }
 
-/**
- * @brief  Soft interrupt wakeup dsp.
- * @return HAL_Status
- */
 HAL_Status HAL_DSP_SoftWakeup(void)
 {
     WRITE_REG_MASK_WE(PMU->DSPAPM_CON, PMU_DSPAPM_CON_PD_DSP_SFT_WAKEUP_MASK, 0);
@@ -267,12 +190,6 @@ HAL_Status HAL_DSP_SoftWakeup(void)
     return HAL_OK;
 }
 
-/**
- * @brief  Set tcm work mode for retention or power down.
- * @param  tcmSel: tcm "|" combination, such as DSP_DTCM0 | DSP_ITCM.
- * @param  mode: work mode in eDSP_tCMMODE.
- * @return HAL_Status
- */
 HAL_Status HAL_DSP_SetTcmMode(uint32_t tcmSel, eDSP_tcmMode mode)
 {
     uint32_t sel;
@@ -291,53 +208,12 @@ HAL_Status HAL_DSP_SetTcmMode(uint32_t tcmSel, eDSP_tcmMode mode)
 
     return HAL_OK;
 }
-#else
-HAL_Status HAL_DSP_SetTcmMode(uint32_t tcmSel, eDSP_tcmMode mode)
-{
-    return HAL_OK;
-}
 
-HAL_Status HAL_DSP_SoftWakeup(void)
-{
-    return HAL_OK;
-}
-
-HAL_Status HAL_DSP_WaitForPowerSt(eDSP_powerSt status, uint32_t timeout)
-{
-    return HAL_OK;
-}
-#endif
-
-/**
- * @brief  Init dsp and deassert reset.
- * @param  dsp: the handle of dsp.
- * @return HAL_Status
- */
 HAL_Status HAL_DSP_Init(struct DSP_DEV *dsp)
 {
     dsp->ops = &dspOps;
     dsp->grfReg = (struct GRF_REG *)(GRF_BASE);
     dsp->resetFlag = DSP_RESET_MODE_RESET_ALL_CLK;
-
-#if defined(RKMCU_KOALA)
-    /*
-     * check the value of CRU_SOFTRST4_CON,
-     * the default value of CRU_SOFTRST4_CON is 0x06,
-     * if m3 want use DTCM of dsp, it will reset dsp and
-     * reassert reset in startup_koala.S. So here,
-     * we check the value of CRU_SOFTRST4_CON to judge
-     * whether DTCM is used by m3
-     */
-    uint32_t setMask = CRU_SOFTRST4_CON_ARESETN_DSP_NOC_REQ_MASK |
-                       CRU_SOFTRST4_CON_RESETN_DSP_REQ_MASK |
-                       CRU_SOFTRST4_CON_RESETN_DSP_EXTCORE_REQ_MASK;
-    int value = READ_BIT(CRU->CRU_SOFTRST_CON[4], setMask);
-
-    /* value 0 means the dsp already be reseted and reasserted reset*/
-    if (value == 0) {
-        dsp->resetFlag = DSP_RESET_MODE_NOT_RESET;
-    }
-#endif
 
     /* Deassert reset */
     HAL_DSP_Enable(dsp, 0);
@@ -345,11 +221,6 @@ HAL_Status HAL_DSP_Init(struct DSP_DEV *dsp)
     return HAL_OK;
 }
 
-/**
- * @brief  DeInit dsp.
- * @param  dsp: the handle of dsp.
- * @return HAL_Status
- */
 HAL_Status HAL_DSP_DeInit(struct DSP_DEV *dsp)
 {
     dsp->ops = NULL;
@@ -364,12 +235,6 @@ HAL_Status HAL_DSP_DeInit(struct DSP_DEV *dsp)
  *  @{
  */
 
-/**
- * @brief  Enable dsp.
- * @param  dsp: the handle of dsp.
- * @param  altAddr: alternate reset vector address.
- * @return HAL_Status
- */
 HAL_Status HAL_DSP_Enable(struct DSP_DEV *dsp, uint32_t altAddr)
 {
     uint32_t setMask = 0;
@@ -378,12 +243,6 @@ HAL_Status HAL_DSP_Enable(struct DSP_DEV *dsp, uint32_t altAddr)
     WRITE_REG_MASK_WE(GRF->DSP_CON0, GRF_DSP_CON0_RUNSTALL_MASK,
                       GRF_DSP_CON0_RUNSTALL_MASK);
     /* Set dsp reset */
-#if defined(RKMCU_RK2206)
-    setMask = CRU_SOFTRST_CON04_DRESETN_HIFI3_MASK |
-              CRU_SOFTRST_CON04_BRESETN_HIFI3_MASK |
-              CRU_SOFTRST_CON04_ARESETN_HIFI3_NIU_MASK;
-    WRITE_REG_MASK_WE(CRU->CRU_SOFTRST_CON[4], setMask, setMask);
-#elif (defined(RKMCU_RK2108) || defined(RKMCU_PISCES))
     if (dsp->resetFlag == DSP_RESET_MODE_RESET_ALL_CLK) {
         setMask = CRU_CRU_SOFTRST_CON00_DRESETN_DSP_MASK |
                   CRU_CRU_SOFTRST_CON00_BRESETN_DSP_MASK |
@@ -394,27 +253,6 @@ HAL_Status HAL_DSP_Enable(struct DSP_DEV *dsp, uint32_t altAddr)
                   CRU_CRU_SOFTRST_CON00_BRESETN_DSP_MASK;
     }
     WRITE_REG_MASK_WE(CRU->CRU_SOFTRST_CON[0], setMask, setMask);
-#elif defined(RKMCU_KOALA)
-    /*
-     * dsp->resetFlag == DSP_RESET_MODE_NOT_RESET means the
-     * m3 is using the DTCM(the address is 0x30240000U ~ 0x3027ffffU) of dsp,
-     * so do not reset dsp here.
-     */
-    if (dsp->resetFlag != DSP_RESET_MODE_NOT_RESET) {
-        if (dsp->resetFlag == DSP_RESET_MODE_RESET_ALL_CLK) {
-            setMask = CRU_SOFTRST4_CON_ARESETN_DSP_NOC_REQ_MASK |
-                      CRU_SOFTRST4_CON_RESETN_DSP_REQ_MASK |
-                      CRU_SOFTRST4_CON_RESETN_DSP_EXTCORE_REQ_MASK;
-            dsp->resetFlag = DSP_RESET_MODE_RESET_PART_CLK;
-        } else {
-            setMask = CRU_SOFTRST4_CON_RESETN_DSP_REQ_MASK |
-                      CRU_SOFTRST4_CON_RESETN_DSP_EXTCORE_REQ_MASK;
-        }
-
-        // reset dsp
-        WRITE_REG_MASK_WE(CRU->CRU_SOFTRST_CON[4], setMask, setMask);
-    }
-#endif
 
     if (altAddr) {
         WRITE_REG_MASK_WE(GRF->DSP_CON0, GRF_DSP_CON0_STATVECTORSEL_MASK,
@@ -423,30 +261,16 @@ HAL_Status HAL_DSP_Enable(struct DSP_DEV *dsp, uint32_t altAddr)
     }
 
     /* Deassert reset */
-#if (defined(RKMCU_RK2206) || defined(RKMCU_KOALA))
-    WRITE_REG_MASK_WE(CRU->CRU_SOFTRST_CON[4], setMask, 0);
-#elif (defined(RKMCU_RK2108) || defined(RKMCU_PISCES))
     WRITE_REG_MASK_WE(CRU->CRU_SOFTRST_CON[0], setMask, 0);
-#endif
 
     return HAL_OK;
 }
 
-/**
- * @brief  Disable dsp.
- * @param  dsp: the handle of dsp.
- * @return HAL_Status
- */
 HAL_Status HAL_DSP_Disable(struct DSP_DEV *dsp)
 {
     return HAL_OK;
 }
 
-/**
- * @brief  Start dsp.
- * @param  dsp: the handle of dsp.
- * @return HAL_Status
- */
 HAL_Status HAL_DSP_Start(struct DSP_DEV *dsp)
 {
     WRITE_REG_MASK_WE(GRF->DSP_CON0, GRF_DSP_CON0_RUNSTALL_MASK, 0);
@@ -454,11 +278,6 @@ HAL_Status HAL_DSP_Start(struct DSP_DEV *dsp)
     return HAL_OK;
 }
 
-/**
- * @brief  Stop dsp.
- * @param  dsp: the handle of dsp.
- * @return HAL_Status
- */
 HAL_Status HAL_DSP_Stop(struct DSP_DEV *dsp)
 {
     WRITE_REG_MASK_WE(GRF->DSP_CON0, GRF_DSP_CON0_RUNSTALL_MASK,
