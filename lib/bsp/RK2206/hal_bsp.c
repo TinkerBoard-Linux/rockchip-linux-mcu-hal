229f3c372842b8c073e8059d7549370fa4756239
Jianqun Xu <jay.xu@rock-chips.com>
2019-11-01 09:45:20 +0800

    lib: bsp: RK2206: fix dataWidth of dwdma to DMA_SLAVE_BUSWIDTH_4_BYTES
    Change-Id: I51783292c2de40db28af84953a47ae3f8d4581a7
    Signed-off-by: Jianqun Xu <jay.xu@rock-chips.com>

--- lib/bsp/RK2206/hal_bsp.c
+++ lib/bsp/RK2206/hal_bsp.c
@@ -43,7 +43,7 @@ struct HAL_DWDMA_DEV g_dwDmaDev =
     .irq[3] = DMAC_CH3_IRQn,
     .irq[4] = DMAC_CH4_IRQn,
     .irq[5] = DMAC_CH5_IRQn,
-    .dataWidth = 4, /**< bytes */
+    .dataWidth = DMA_SLAVE_BUSWIDTH_4_BYTES,
     .blockSize = 0xfff, /** ref to trm */
 };
 #endif
