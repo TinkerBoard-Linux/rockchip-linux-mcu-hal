386aeaac8b55d56faaa264473c2699131bd32a2d
Jason Zhu <jason.zhu@rock-chips.com>
2020-03-17 10:04:51 +0800

    rv1126: enable uart module to print debug infomation
    Signed-off-by: Jason Zhu <jason.zhu@rock-chips.com>
    Change-Id: I83a5a19315270c20ddfd71cf43420f3711eda9ca

--- project/rv1126-riscv/src/main.c
+++ project/rv1126-riscv/src/main.c
@@ -16,9 +16,28 @@
 /********************* Private Function Definition ***************************/
 
 /********************* Public Function Definition ****************************/
+static struct UART_REG *pUart = UART2;
+
+int __wrap_puts(const char *s)
+{
+    while (*s != '\0') {
+        if (*s == '\n')
+            HAL_UART_SerialOutChar(pUart, '\r');
+
+        HAL_UART_SerialOutChar(pUart, *s);
+        ++s;
+    }
+
+    return 0;
+}
 
 int main(void)
 {
+    /* Uart is initialized by pre-loader,
+     * use it to print debug info directly.
+     */
+    printf("Hello RISC-V\n");
+
     return 0;
 }
 
