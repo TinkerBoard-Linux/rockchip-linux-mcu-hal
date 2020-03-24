4bffc34d8f0303ef6fb2defec0c5beed5f34e628
Jon Lin <jon.lin@rock-chips.com>
2020-03-24 09:54:21 +0800

    tools: add build.mk helper
    Change-Id: I74974a4153bac72fc4682ad1332769e69ae7f20e
    Signed-off-by: Jon Lin <jon.lin@rock-chips.com>

---
+++ tools/build_lib.mk
@@ -0,0 +1,19 @@
+# SPDX-License-Identifier: BSD-3-Clause */
+
+# Copyright (c) 2020 Fuzhou Rockchip Electronics Co., Ltd
+
+#-------------------------------------------------------------------------
+# set HAL_PATH and SOC
+#-------------------------------------------------------------------------
+HAL_LIB_INC += \
+-I"$(HAL_PATH)/lib/hal/inc" \
+-I"$(HAL_PATH)/lib/bsp/$(SOC)" \
+-I"$(HAL_PATH)/lib/CMSIS/Device/$(SOC)/Include" \
+-I"$(HAL_PATH)/lib/CMSIS/Core/Include" \
+-I"$(HAL_PATH)/lib/CMSIS/Device" \
+
+HAL_LIB_SRC += \
+    $(HAL_PATH)/lib/hal/src \
+    $(HAL_PATH)/lib/hal/src/* \
+    $(HAL_PATH)/lib/bsp/$(SOC) \
+    $(HAL_PATH)/lib/CMSIS/Device/$(SOC)/Source/Templates \
