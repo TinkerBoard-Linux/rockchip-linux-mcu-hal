cd6c796ef74a1ffde1c57457b5dba9395bf52b9a
Elaine Zhang <zhangqing@rock-chips.com>
2020-09-11 17:06:38 +0800

    lib: hal: cru: Update swallow clk code
    Signed-off-by: Elaine Zhang <zhangqing@rock-chips.com>
    Change-Id: I377f089178913872faa8f0539a9af11a4696adeb

--- lib/CMSIS/Device/Swallow/Include/soc.h
+++ lib/CMSIS/Device/Swallow/Include/soc.h
@@ -155,29 +155,307 @@ typedef enum
 /*****************************************FSPI*******************************************/
 #define FSPI_CHIP_CNT                                       (0x1U)
 
+/********Name=SOFTRST_CON00,Offset=0x400********/
+#define SRST_H_MCU_BUS_AC   1U
+#define SRST_MCU_AC         2U
+#define SRST_P_LOGIC_BUS_AC 3U
+/********Name=SOFTRST_CON01,Offset=0x404********/
+#define SRST_H_MCU_BUS_NIU 17U
+#define SRST_MCU           18U
+#define SRST_H_INTMEM0     19U
+#define SRST_H_INTMEM1     20U
+#define SRST_H_INTMEM2     21U
+#define SRST_H_ROM         22U
+#define SRST_H_SFC         23U
+#define SRST_H_SFC_XIP     24U
+#define SRST_H_USB         25U
+#define SRST_A_VIPCAP      28U
+#define SRST_H_VIPCAP      29U
+#define SRST_H_ISP         31U
+/********Name=SOFTRST_CON02,Offset=0x408********/
+#define SRST_H_JPEG_ENC0     32U
+#define SRST_H_JPEG_ENC1     33U
+#define SRST_H_LOGIC_BUS_NIU 34U
+#define SRST_H_DMAC          35U
+#define SRST_H_JHASH_M       36U
+#define SRST_H_JHASH_S       37U
+#define SRST_H_HOST_ARB      38U
+#define SRST_A_JPEG_BUS_NIU  41U
+#define SRST_A_JPEG_ENC0     42U
+#define SRST_A_JPEG_ENC1     43U
+/********Name=SOFTRST_CON03,Offset=0x40C********/
+#define SRST_P_LOGIC_BUS_NIU  49U
+#define SRST_P_UART           50U
+#define SRST_P_I2C0           51U
+#define SRST_P_I2C1           52U
+#define SRST_P_PWM            53U
+#define SRST_P_SPI0           54U
+#define SRST_P_SPI1           55U
+#define SRST_P_SARADC_CONTROL 56U
+#define SRST_P_TIMER          57U
+#define SRST_P_WDT            58U
+#define SRST_P_CSI2HOST       59U
+#define SRST_P_BUFFER_MANAGE  60U
+#define SRST_P_INTCTRL        61U
+/********Name=SOFTRST_CON04,Offset=0x410********/
+#define SRST_P_GPIO0  68U
+#define SRST_P_GPIO1  69U
+#define SRST_P_GRF    72U
+#define SRST_P_USBGRF 73U
+#define SRST_DB_GPIO0 74U
+#define SRST_DB_GPIO1 75U
+#define SRST_USBPHY   77U
+/********Name=SOFTRST_CON05,Offset=0x414********/
+#define SRST_I2C0      80U
+#define SRST_I2C1      81U
+#define SRST_SPI0      82U
+#define SRST_SPI1      83U
+#define SRST_PWM       84U
+#define SRST_TIMER0    86U
+#define SRST_TIMER1    87U
+#define SRST_TIMER2    88U
+#define SRST_TIMER3    89U
+#define SRST_TIMER4    90U
+#define SRST_TIMER5    91U
+#define SRST_MCU_JTRST 92U
+#define SRST_XIP_SFC   93U
+/********Name=SOFTRST_CON06,Offset=0x418********/
+#define SRST_ISP      97U
+#define SRST_D_VIPCAP 98U
+#define SRST_I_VIPCAP 99U
+#define SRST_UTMI     101U
+#define SRST_USB_ADP  102U
+#define SRST_S_UART   106U
+/********Name=SOFTRST_CON07,Offset=0x41C********/
+#define SRST_WBUFFER_CH0_MANAGE0 112U
+#define SRST_WBUFFER_CH0_MANAGE1 113U
+#define SRST_WBUFFER_CH1_MANAGE0 114U
+#define SRST_WBUFFER_CH2_MANAGE0 115U
+#define SRST_WBUFFER_CH2_MANAGE1 116U
+#define SRST_WBUFFER_CH3_MANAGE0 117U
+#define SRST_RBUFFER_CH0_MANAGE0 118U
+#define SRST_RBUFFER_CH0_MANAGE1 119U
+#define SRST_RBUFFER_CH1_MANAGE0 120U
+#define SRST_RBUFFER_CH1_MANAGE1 121U
+
+/********Name=GATE_CON00,Offset=0x300********/
+#define CLK_32K_GATE              1U
+#define CLK_GPLL_MUX_DIV_NP5_GATE 4U
+#define HCLK_MCU_BUS_PLL_GATE     5U
+/********Name=GATE_CON01,Offset=0x304********/
+#define HCLK_LOGIC_BUS_GATE     16U
+#define HCLK_MCU_BUS_NIU_GATE   17U
+#define CLK_MCU_GATE            18U
+#define HCLK_INTMEM0_LOGIC_GATE 19U
+#define HCLK_INTMEM1_LOGIC_GATE 20U
+#define HCLK_INTMEM2_LOGIC_GATE 21U
+#define HCLK_ROM_GATE           22U
+#define HCLK_SFC_GATE           23U
+#define HCLK_SFC_XIP_GATE       24U
+#define HCLK_USB_GATE           25U
+#define HCLK_USB_PMU_GATE       26U
+#define ACLK_VIPCAP_GATE        28U
+#define HCLK_VIPCAP_GATE        29U
+#define ACLK_ISP_GATE           30U
+#define HCLK_ISP_GATE           31U
+/********Name=GATE_CON02,Offset=0x308********/
+#define HCLK_JPEG_ENC0_GATE     32U
+#define HCLK_JPEG_ENC1_GATE     33U
+#define HCLK_LOGIC_BUS_NIU_GATE 34U
+#define HCLK_DMAC_GATE          35U
+#define HCLK_JHASH_M_GATE       36U
+#define HCLK_JHASH_S_GATE       37U
+#define HCLK_HOST_ARB_GATE      38U
+#define ACLK_JPEG_BUS_PLL_GATE  40U
+#define ACLK_JPEG_BUS_NIU_GATE  41U
+#define ACLK_JPEG_ENC0_GATE     42U
+#define ACLK_JPEG_ENC1_GATE     43U
+/********Name=GATE_CON03,Offset=0x30C********/
+#define PCLK_LOGIC_BUS_PLL_GATE  48U
+#define PCLK_LOGIC_BUS_NIU_GATE  49U
+#define PCLK_UART_GATE           50U
+#define PCLK_I2C0_GATE           51U
+#define PCLK_I2C1_GATE           52U
+#define PCLK_PWM_GATE            53U
+#define PCLK_SPI0_GATE           54U
+#define PCLK_SPI1_GATE           55U
+#define PCLK_SARADC_CONTROL_GATE 56U
+#define PCLK_TIMER_GATE          57U
+#define PCLK_WDT_GATE            58U
+#define PCLK_CSI2HOST_GATE       59U
+#define PCLK_BUFFER_MANAGE_GATE  60U
+#define PCLK_INTCTRL_GATE        61U
+/********Name=GATE_CON04,Offset=0x310********/
+#define PCLK_GPIO0_GATE       68U
+#define PCLK_GPIO1_GATE       69U
+#define PCLK_CRU_GATE         70U
+#define PCLK_GRF_GATE         72U
+#define PCLK_USBGRF_GATE      73U
+#define DBCLK_GPIO0_GATE      74U
+#define DBCLK_GPIO1_GATE      75U
+#define CLK_SARADC_GATE       76U
+#define CLK_MIPIPHY0_CFG_GATE 78U
+#define CLK_MIPIPHY1_CFG_GATE 79U
+/********Name=GATE_CON05,Offset=0x314********/
+#define CLK_I2C0_PLL_GATE        80U
+#define CLK_I2C1_PLL_GATE        81U
+#define CLK_SPI0_PLL_GATE        82U
+#define CLK_SPI1_PLL_GATE        83U
+#define CLK_PWM_PLL_GATE         84U
+#define CLK_TIMER_PLL_GATE       85U
+#define CLK_TIMER0_GATE          86U
+#define CLK_TIMER1_GATE          87U
+#define CLK_TIMER2_GATE          88U
+#define CLK_TIMER3_GATE          89U
+#define CLK_TIMER4_GATE          90U
+#define CLK_TIMER5_GATE          91U
+#define CLK_XIP_SFC_DT50_GATE    93U
+#define CLK_PWM_CAPTURE_PLL_GATE 94U
+#define RTCCLK_MCU_GATE          95U
+/********Name=GATE_CON06,Offset=0x318********/
+#define CLK_WDT_PLL_GATE     96U
+#define CLK_ISP_PLL_GATE     97U
+#define DCLK_VIPCAP_PLL_GATE 98U
+#define CLK_VIP_OUT_PLL_GATE 100U
+#define CLK_USB_ADP_GATE     102U
+#define OUTCLOCK_TEST_GATE   103U
+#define CLK_UART_PLL_GATE    104U
+#define CLK_UART_FRAC_GATE   105U
+#define SCLK_UART_GATE       106U
+
+/********Name=CLKSEL_CON00,Offset=0x100********/
+#define HCLK_MCU_BUS_DIV     0x07000000U
+#define CLK_GPLL_MUX_NP5_DIV 0x04080000U
+/********Name=CLKSEL_CON01,Offset=0x104********/
+#define CLK_32K_FRAC_DIV_DIV 0x20000001U
+/********Name=CLKSEL_CON02,Offset=0x108********/
+#define HCLK_LOGIC_BUS_DIV 0x04000002U
+#define ACLK_JPEG_BUS_DIV  0x05080002U
+/********Name=CLKSEL_CON03,Offset=0x10C********/
+#define PCLK_LOGIC_BUS_DIV 0x05000003U
+#define CLK_SARADC_DIV     0x08080003U
+/********Name=CLKSEL_CON04,Offset=0x110********/
+#define CLK_I2C0_DIV 0x06000004U
+#define CLK_I2C1_DIV 0x06080004U
+/********Name=CLKSEL_CON05,Offset=0x114********/
+#define CLK_SPI0_DIV 0x06000005U
+#define CLK_SPI1_DIV 0x06080005U
+/********Name=CLKSEL_CON06,Offset=0x118********/
+#define CLK_PWM_DIV   0x06000006U
+#define CLK_TIMER_DIV 0x06080006U
+/********Name=CLKSEL_CON07,Offset=0x11C********/
+#define CLK_XIP_SFC_DIV 0x06000007U
+#define CLK_ISP_DIV     0x06080007U
+/********Name=CLKSEL_CON08,Offset=0x120********/
+#define DCLK_VIPCAP_DIV 0x06000008U
+#define CLK_VIP_OUT_DIV 0x06080008U
+/********Name=CLKSEL_CON09,Offset=0x124********/
+#define OUTCLOCK_TEST_DIV 0x04000009U
+/********Name=CLKSEL_CON10,Offset=0x128********/
+#define CLK_UART_DIV_DIV 0x0500000AU
+/********Name=CLKSEL_CON11,Offset=0x12C********/
+#define CLK_UART_FRAC_DIV_DIV 0x2000000BU
+
+/********Name=CLKSEL_CON00,Offset=0x100********/
+#define HCLK_MCU_BUS_SEL                  0x020E0000U
+#define HCLK_MCU_BUS_SEL_CLK_GPLL_MUX     0U
+#define HCLK_MCU_BUS_SEL_CLK_GPLL_MUX_NP5 1U
+#define HCLK_MCU_BUS_SEL_CLK_USBPLL_MUX   2U
+#define HCLK_MCU_BUS_SEL_RESERVED         3U
+/********Name=CLKSEL_CON01,Offset=0x104********/
+/********Name=CLKSEL_CON02,Offset=0x108********/
+#define ACLK_JPEG_BUS_SEL                  0x010F0002U
+#define ACLK_JPEG_BUS_SEL_CLK_GPLL_MUX     0U
+#define ACLK_JPEG_BUS_SEL_CLK_GPLL_MUX_NP5 1U
+/********Name=CLKSEL_CON03,Offset=0x10C********/
+/********Name=CLKSEL_CON04,Offset=0x110********/
+#define CLK_I2C0_SEL                  0x02060004U
+#define CLK_I2C0_SEL_CLK_GPLL_MUX     0U
+#define CLK_I2C0_SEL_CLK_GPLL_MUX_NP5 1U
+#define CLK_I2C0_SEL_XIN_OSC0_FUNC    2U
+#define CLK_I2C1_SEL                  0x020E0004U
+#define CLK_I2C1_SEL_CLK_GPLL_MUX     0U
+#define CLK_I2C1_SEL_CLK_GPLL_MUX_NP5 1U
+#define CLK_I2C1_SEL_XIN_OSC0_FUNC    2U
+/********Name=CLKSEL_CON05,Offset=0x114********/
+#define CLK_SPI0_SEL               0x01070005U
+#define CLK_SPI0_SEL_CLK_GPLL_MUX  0U
+#define CLK_SPI0_SEL_XIN_OSC0_FUNC 1U
+#define CLK_SPI1_SEL               0x010F0005U
+#define CLK_SPI1_SEL_CLK_GPLL_MUX  0U
+#define CLK_SPI1_SEL_XIN_OSC0_FUNC 1U
+/********Name=CLKSEL_CON06,Offset=0x118********/
+#define CLK_PWM_SEL                 0x01070006U
+#define CLK_PWM_SEL_CLK_GPLL_MUX    0U
+#define CLK_PWM_SEL_XIN_OSC0_FUNC   1U
+#define CLK_TIMER_SEL               0x010F0006U
+#define CLK_TIMER_SEL_CLK_GPLL_MUX  0U
+#define CLK_TIMER_SEL_XIN_OSC0_FUNC 1U
+/********Name=CLKSEL_CON07,Offset=0x11C********/
+#define CLK_XIP_SFC_SEL                0x02060007U
+#define CLK_XIP_SFC_SEL_CLK_GPLL_MUX   0U
+#define CLK_XIP_SFC_SEL_CLK_USBPLL_MUX 1U
+#define CLK_XIP_SFC_SEL_XIN_OSC0_FUNC  2U
+/********Name=CLKSEL_CON08,Offset=0x120********/
+#define CLK_VIP_OUT_SEL                0x020E0008U
+#define CLK_VIP_OUT_SEL_CLK_GPLL_MUX   0U
+#define CLK_VIP_OUT_SEL_CLK_USBPLL_MUX 1U
+#define CLK_VIP_OUT_SEL_XIN_OSC0_FUNC  2U
+/********Name=CLKSEL_CON09,Offset=0x124********/
+#define OUTCLOCK_TEST_SEL                0x04040009U
+#define OUTCLOCK_TEST_SEL_XIN_OSC0_FUNC  0U
+#define OUTCLOCK_TEST_SEL_CLK_32K        1U
+#define OUTCLOCK_TEST_SEL_CLK_GPLL       2U
+#define OUTCLOCK_TEST_SEL_CLK_USBPLL     3U
+#define OUTCLOCK_TEST_SEL_USB_PHYCLK     4U
+#define OUTCLOCK_TEST_SEL_HCLK_MCU_BUS   5U
+#define OUTCLOCK_TEST_SEL_ACLK_JPEG_BUS  6U
+#define OUTCLOCK_TEST_SEL_PCLK_LOGIC_BUS 7U
+#define OUTCLOCK_TEST_SEL_CLK_I2C0       8U
+#define OUTCLOCK_TEST_SEL_CLK_I2C1       9U
+#define OUTCLOCK_TEST_SEL_CLK_ISP        10U
+#define OUTCLOCK_TEST_SEL_MIPI_PHYCLK0   11U
+#define OUTCLOCK_TEST_SEL_MIPI_PHYCLK1   12U
+#define OUTCLOCK_TEST_SEL_DCLK_VIPCAP    13U
+#define OUTCLOCK_TEST_SEL_CLK_XIP_SFC    14U
+#define OUTCLOCK_TEST_SEL_SCLK_UART      15U
+/********Name=CLKSEL_CON10,Offset=0x128********/
+#define CLK_UART_DIV_SEL                0x0107000AU
+#define CLK_UART_DIV_SEL_CLK_GPLL_MUX   0U
+#define CLK_UART_DIV_SEL_XIN_OSC0_FUNC  1U
+#define SCLK_UART_SEL                   0x010F000AU
+#define SCLK_UART_SEL_CLK_UART_DIV      0U
+#define SCLK_UART_SEL_CLK_UART_FRAC_DIV 1U
+/********Name=CLKSEL_CON11,Offset=0x12C********/
+
+#define CLK(mux, div) \
+    (((mux) & 0x0F0F00FFU) | (((div) & 0xFFU) << 8) | (((div) & 0x0F0F0000U) << 4))
+
 #ifndef __ASSEMBLY__
 typedef enum CLOCK_Name {
     CLK_INVALID = 0U,
     PLL_GPLL,
-    PLL_CPLL,
-    CLK_UART0_SRC,
-    CLK_UART0_FRAC,
-    CLK_UART0,
-    CLK_I2C0,
-    CLK_I2C1,
-    ACLK_DMAC,
-    CLK_GPIO_DBG0,
-    CLK_GPIO_DBG1,
-    PCLK_ALIVE,
-    HCLK_ALIVE,
-    CLK_SPI0,
-    CLK_SPI1,
-    SCLK_SFC_SRC,
-    CLK_PWM,
-    HCLK_LOGIC,
-    PCLK_LOGIC,
-    ACLK_LOGIC,
-    CLK_32K,
+    PLL_GPLL_NP5   = CLK(0U, CLK_GPLL_MUX_NP5_DIV),
+    PCLK_LOGIC_BUS = CLK(0U, PCLK_LOGIC_BUS_DIV),
+    CLK_SARADC     = CLK(0U, CLK_SARADC_DIV),
+    ACLK_JPEG_BUS  = CLK(ACLK_JPEG_BUS_SEL, ACLK_JPEG_BUS_DIV),
+    HCLK_MCU_BUS   = CLK(HCLK_MCU_BUS_SEL, HCLK_MCU_BUS_DIV),
+    HCLK_LOGIC_BUS = CLK(0U, HCLK_LOGIC_BUS_DIV),
+    DCLK_VIPCAP    = CLK(0U, DCLK_VIPCAP_DIV),
+    CLK_ISP        = CLK(0U, CLK_ISP_DIV),
+    CLK_I2C0       = CLK(CLK_I2C0_SEL, CLK_I2C0_DIV),
+    CLK_I2C1       = CLK(CLK_I2C1_SEL, CLK_I2C1_DIV),
+    CLK_SPI0       = CLK(CLK_SPI0_SEL, CLK_SPI0_DIV),
+    CLK_SPI1       = CLK(CLK_SPI1_SEL, CLK_SPI1_DIV),
+    CLK_PWM        = CLK(CLK_PWM_SEL, CLK_PWM_DIV),
+    CLK_TIMER      = CLK(CLK_TIMER_SEL, CLK_TIMER_DIV),
+    CLK_XIP_SFC    = CLK(CLK_XIP_SFC_SEL, CLK_XIP_SFC_DIV),
+    CLK_VIP_OUT    = CLK(CLK_VIP_OUT_SEL, CLK_VIP_OUT_DIV),
+    OUTCLK_TEST    = CLK(OUTCLOCK_TEST_SEL, OUTCLOCK_TEST_DIV),
+    CLK_UART0_SRC  = CLK(CLK_UART_DIV_SEL, CLK_UART_DIV_DIV),
+    CLK_UART0_FRAC = CLK(0U, CLK_UART_FRAC_DIV_DIV),
+    CLK_UART0      = CLK(SCLK_UART_SEL, 0U),
+    CLK_32K        = CLK(0U, CLK_32K_FRAC_DIV_DIV),
 } eCLOCK_Name;
 #endif /* __ASSEMBLY__ */
 
--- lib/hal/src/cru/hal_cru_swallow.c
+++ lib/hal/src/cru/hal_cru_swallow.c
@@ -21,6 +21,25 @@
 /********************* Private MACRO Definition ******************************/
 
 /********************* Private Structure Definition **************************/
+static struct PLL_CONFIG PLL_TABLE[] = {
+    /* _mhz, _refDiv, _fbDiv, _postdDv1, _postDiv2, _dsmpd, _frac */
+    RK_PLL_RATE(297000000, 1, 99, 8, 1, 1, 0),
+    { /* sentinel */ },
+};
+
+static uint32_t s_gpllFreq;
+static uint32_t s_usbphyFreq = 480000000;
+
+static struct PLL_SETUP GPLL = {
+    .conOffset0 = &(CRU->GPLL_CON[0]),
+    .conOffset1 = &(CRU->GPLL_CON[1]),
+    .conOffset2 = &(CRU->GPLL_CON[2]),
+    .modeOffset = &(CRU->CRU_MODE_CON00),
+    .modeShift = 0,
+    .lockShift = 10,
+    .modeMask = 0x3 << 0,
+    .rateTable = PLL_TABLE,
+};
 
 /********************* Private Variable Definition ***************************/
 
@@ -45,7 +64,54 @@
  */
 static uint32_t HAL_CRU_ClkFracGetFreq(eCLOCK_Name clockName)
 {
-    return 0;
+    uint32_t freq = 0;
+    uint32_t muxSrc = 0, mux = CLK_GET_MUX(clockName);
+    uint32_t divSrc = 0, divFrac;
+    uint32_t n, m, pRate;
+
+    switch (clockName) {
+    case CLK_UART0:
+        muxSrc = CLK_GET_MUX(CLK_UART0_SRC);
+        divSrc = CLK_GET_DIV(CLK_UART0_SRC);
+        divFrac = CLK_GET_DIV(CLK_UART0_FRAC);
+        break;
+    case CLK_32K:
+        divFrac = CLK_GET_DIV(CLK_32K);
+        break;
+    default:
+
+        return HAL_INVAL;
+    }
+
+    n = (CRU->CRU_CLKSEL_CON[CLK_DIV_GET_REG_OFFSET(divFrac)] & 0xffff0000) >> 16;
+    m = CRU->CRU_CLKSEL_CON[CLK_DIV_GET_REG_OFFSET(divFrac)] & 0x0000ffff;
+
+    switch (clockName) {
+    case CLK_UART0:
+        if (HAL_CRU_ClkGetMux(muxSrc)) {
+            pRate = PLL_INPUT_OSC_RATE;
+        } else {
+            pRate = s_gpllFreq;
+        }
+        break;
+    case CLK_32K:
+        pRate = PLL_INPUT_OSC_RATE;
+        freq = (pRate / m) * n;
+
+        return freq;
+    default:
+
+        return HAL_INVAL;
+    }
+
+    pRate = pRate / HAL_CRU_ClkGetDiv(divSrc);
+    if (HAL_CRU_ClkGetMux(mux) == 0) {
+        freq = pRate;
+    } else {
+        freq = (pRate / m) * n;
+    }
+
+    return freq;
 }
 
 /**
@@ -59,7 +125,59 @@ static uint32_t HAL_CRU_ClkFracGetFreq(eCLOCK_Name clockName)
  */
 static HAL_Status HAL_CRU_ClkFracSetFreq(eCLOCK_Name clockName, uint32_t rate)
 {
-    return HAL_OK;
+    uint32_t muxSrc, mux = CLK_GET_MUX(clockName);
+    uint32_t divSrc, divFrac;
+    uint32_t n = 0, m = 0;
+    uint32_t gateId, fracGateId;
+
+    switch (clockName) {
+    case CLK_UART0:
+        muxSrc = CLK_GET_MUX(CLK_UART0_SRC);
+        divSrc = CLK_GET_DIV(CLK_UART0_SRC);
+        divFrac = CLK_GET_DIV(CLK_UART0_FRAC);
+        gateId = CLK_UART_PLL_GATE;
+        fracGateId = CLK_UART_FRAC_GATE;
+        break;
+    case CLK_32K:
+        divFrac = CLK_GET_DIV(CLK_32K);
+        break;
+    default:
+
+        return HAL_INVAL;
+    }
+
+    switch (clockName) {
+    case CLK_UART0:
+        HAL_CRU_ClkEnable(gateId);
+        HAL_CRU_ClkEnable(fracGateId);
+        if ((!(PLL_INPUT_OSC_RATE % rate)) && ((PLL_INPUT_OSC_RATE / rate) < 31)) {
+            HAL_CRU_ClkSetDiv(divSrc, PLL_INPUT_OSC_RATE / rate);
+            HAL_CRU_ClkSetMux(muxSrc, 1);
+            HAL_CRU_ClkSetMux(mux, 0);
+            HAL_CRU_ClkDisable(fracGateId);
+        } else if ((!(s_gpllFreq % rate)) && ((s_gpllFreq / rate) < 31)) {
+            HAL_CRU_ClkSetDiv(divSrc, s_gpllFreq / rate);
+            HAL_CRU_ClkSetMux(muxSrc, 0);
+            HAL_CRU_ClkSetMux(mux, 0);
+            HAL_CRU_ClkDisable(fracGateId);
+        } else {
+            HAL_CRU_FracdivGetConfig(rate, s_gpllFreq, &n, &m);
+            HAL_CRU_ClkSetDiv(divSrc, 1);
+            HAL_CRU_ClkSetMux(muxSrc, 0);
+            CRU->CRU_CLKSEL_CON[CLK_DIV_GET_REG_OFFSET(divFrac)] = (n << 16) | m;
+            HAL_CRU_ClkSetMux(mux, 1);
+        }
+
+        return HAL_OK;
+    case CLK_32K:
+        HAL_CRU_FracdivGetConfig(rate, PLL_INPUT_OSC_RATE, &n, &m);
+        CRU->CRU_CLKSEL_CON[CLK_DIV_GET_REG_OFFSET(divFrac)] = (n << 16) | m;
+
+        return HAL_OK;
+    default:
+
+        return HAL_INVAL;
+    }
 }
 
 /**
@@ -71,7 +189,87 @@ static HAL_Status HAL_CRU_ClkFracSetFreq(eCLOCK_Name clockName, uint32_t rate)
 HAL_SECTION_SRAM_CODE
 uint32_t HAL_CRU_ClkGetFreq(eCLOCK_Name clockName)
 {
-    return 0;
+    uint32_t freq;
+    uint32_t clkMux = CLK_GET_MUX(clockName);
+    uint32_t clkDiv = CLK_GET_DIV(clockName);
+    uint32_t pRate = s_gpllFreq;
+
+    if (!s_gpllFreq) {
+        s_gpllFreq = HAL_CRU_GetPllFreq(&GPLL);
+    }
+    switch (clockName) {
+    case PLL_GPLL:
+        freq = HAL_CRU_GetPllFreq(&GPLL);
+        s_gpllFreq = freq;
+
+        return freq;
+    case PLL_GPLL_NP5:
+        freq = (pRate * 2) / ((HAL_CRU_ClkGetDiv(clkDiv) - 1) * 2 + 3);
+
+        return freq;
+    case HCLK_LOGIC_BUS:
+        pRate = HAL_CRU_ClkGetFreq(HCLK_MCU_BUS);
+        break;
+    case CLK_SARADC:
+        pRate = PLL_INPUT_OSC_RATE;
+        break;
+    case CLK_UART0:
+    case CLK_32K:
+        freq = HAL_CRU_ClkFracGetFreq(clockName);
+
+        return freq;
+    case HCLK_MCU_BUS:
+        if (HAL_CRU_ClkGetMux(clkMux) == 2) {
+            pRate = s_usbphyFreq;
+        } else if (HAL_CRU_ClkGetMux(clkMux) == 1) {
+            pRate = HAL_CRU_ClkGetFreq(PLL_GPLL_NP5);
+        }
+        break;
+    case ACLK_JPEG_BUS:
+        if (HAL_CRU_ClkGetMux(clkMux)) {
+            pRate = HAL_CRU_ClkGetFreq(PLL_GPLL_NP5);
+        }
+        break;
+    case CLK_SPI0:
+    case CLK_SPI1:
+    case CLK_PWM:
+    case CLK_TIMER:
+        if (HAL_CRU_ClkGetMux(clkMux)) {
+            pRate = PLL_INPUT_OSC_RATE;
+        }
+        break;
+    case CLK_I2C0:
+    case CLK_I2C0:
+        if (HAL_CRU_ClkGetMux(clkMux) == 2) {
+            pRate = PLL_INPUT_OSC_RATE;
+        } else if (HAL_CRU_ClkGetMux(clkMux) == 1) {
+            pRate = HAL_CRU_ClkGetFreq(PLL_GPLL_NP5);
+        }
+        break;
+    case CLK_XIP_SFC:
+    case CLK_VIP_OUT:
+        if (HAL_CRU_ClkGetMux(clkMux) == 2) {
+            pRate = PLL_INPUT_OSC_RATE;
+        } else if (HAL_CRU_ClkGetMux(clkMux) == 1) {
+            pRate = s_usbphyFreq;
+        }
+        break;
+    default:
+
+        break;
+    }
+
+    if ((clkMux == 0) && (clkDiv == 0)) {
+        return 0;
+    }
+
+    if (clkDiv) {
+        freq = pRate / (HAL_CRU_ClkGetDiv(clkDiv));
+    } else {
+        freq = pRate;
+    }
+
+    return freq;
 }
 
 /**
@@ -83,6 +281,142 @@ uint32_t HAL_CRU_ClkGetFreq(eCLOCK_Name clockName)
  */
 HAL_Status HAL_CRU_ClkSetFreq(eCLOCK_Name clockName, uint32_t rate)
 {
+    HAL_Status error = HAL_OK;
+    uint32_t clkMux = CLK_GET_MUX(clockName), mux = 0;
+    uint32_t clkDiv = CLK_GET_DIV(clockName), div = 0;
+    uint32_t pRate = s_gpllFreq;
+
+    if (!s_gpllFreq) {
+        s_gpllFreq = HAL_CRU_GetPllFreq(&GPLL);
+    }
+
+    switch (clockName) {
+    case PLL_GPLL:
+        error = HAL_CRU_SetPllFreq(&GPLL, rate);
+        s_gpllFreq = HAL_CRU_GetPllFreq(&GPLL);
+
+        return error;
+    case PLL_GPLL_NP5:
+        div = (HAL_DIV_ROUND_UP(pRate * 2, rate) - 3) / 2;
+        HAL_CRU_ClkSetDiv(clkDiv, div + 1);
+
+        return error;
+    case HCLK_LOGIC_BUS:
+        pRate = HAL_CRU_ClkGetFreq(HCLK_MCU_BUS);
+        break;
+    case CLK_SARADC:
+        pRate = PLL_INPUT_OSC_RATE;
+        break;
+    case CLK_UART0:
+    case CLK_32K:
+        error = HAL_CRU_ClkFracSetFreq(clockName, rate);
+
+        return error;
+    case HCLK_MCU_BUS:
+        if (!(s_usbphyFreq % rate)) {
+            pRate = s_usbphyFreq;
+            mux = HCLK_MCU_BUS_SEL_CLK_USBPLL_MUX;
+        } else if (!(HAL_CRU_ClkGetFreq(PLL_GPLL_NP5) % rate)) {
+            pRate = HAL_CRU_ClkGetFreq(PLL_GPLL_NP5);
+            mux = HCLK_MCU_BUS_SEL_CLK_GPLL_MUX_NP5;
+        } else {
+            pRate = s_gpllFreq;
+            mux = HCLK_MCU_BUS_SEL_CLK_GPLL_MUX;
+        }
+        break;
+    case ACLK_JPEG_BUS:
+        if (!(HAL_CRU_ClkGetFreq(PLL_GPLL_NP5) % rate)) {
+            pRate = HAL_CRU_ClkGetFreq(PLL_GPLL_NP5);
+            mux = ACLK_JPEG_BUS_SEL_CLK_GPLL_MUX_NP5;
+        } else {
+            pRate = s_gpllFreq;
+            mux = ACLK_JPEG_BUS_SEL_CLK_GPLL_MUX;
+        }
+        break;
+    case CLK_SPI0:
+    case CLK_SPI1:
+    case CLK_PWM:
+    case CLK_TIMER:
+        if (!(PLL_INPUT_OSC_RATE % rate)) {
+            pRate = PLL_INPUT_OSC_RATE;
+            mux = CLK_SPI0_SEL_XIN_OSC0_FUNC;
+        } else {
+            pRate = s_gpllFreq;
+            mux = CLK_SPI0_SEL_CLK_GPLL_MUX;
+        }
+        break;
+    case CLK_I2C0:
+    case CLK_I2C0:
+        if (!(PLL_INPUT_OSC_RATE % rate)) {
+            pRate = PLL_INPUT_OSC_RATE;
+            mux = CLK_I2C0_SEL_XIN_OSC0_FUNC;
+        } else if (!(HAL_CRU_ClkGetFreq(PLL_GPLL_NP5) % rate)) {
+            pRate = HAL_CRU_ClkGetFreq(PLL_GPLL_NP5);
+            mux = CLK_I2C0_SEL_CLK_GPLL_MUX_NP5;
+        } else {
+            pRate = s_gpllFreq;
+            mux = CLK_I2C0_SEL_CLK_GPLL_MUX;
+        }
+        break;
+    case CLK_XIP_SFC:
+    case CLK_VIP_OUT:
+        if (!(PLL_INPUT_OSC_RATE % rate)) {
+            pRate = PLL_INPUT_OSC_RATE;
+            mux = CLK_XIP_SFC_SEL_XIN_OSC0_FUNC;
+        } else if (!(s_usbphyFreq % rate)) {
+            pRate = s_usbphyFreq;
+            mux = CLK_VIP_OUT_SEL_CLK_USBPLL_MUX;
+        } else {
+            pRate = s_gpllFreq;
+            mux = CLK_XIP_SFC_SEL_CLK_GPLL_MUX;
+        }
+        break;
+    default:
+
+        break;
+    }
+
+    if ((clkMux == 0) && (clkDiv == 0)) {
+        return HAL_INVAL;
+    }
+
+    div = HAL_DIV_ROUND_UP(pRate, rate);
+    if (clkMux) {
+        HAL_CRU_ClkSetMux(clkMux, mux);
+    }
+    if (clkDiv) {
+        HAL_CRU_ClkSetDiv(clkDiv, div);
+    }
+
+    return HAL_OK;
+}
+
+/**
+ * @brief wdt glbrst enable.
+ * @param  wdtType: wdt reset type.
+ * @return HAL_OK.
+ * @attention these APIs allow direct use in the HAL layer.
+ */
+HAL_Status HAL_CRU_WdtGlbRstEnable(eCRU_WdtRstType wdtType)
+{
+    uint32_t mask = 0, val = 0;
+
+    switch (wdtType) {
+    case GLB_RST_FST_WDT0:
+        mask = CRU_GLB_RST_CON_WDT_GLB_SRST_EN_MASK | CRU_GLB_RST_CON_WDT_GLB_SRST_CTRL_MASK;
+        val = (1 << CRU_GLB_RST_CON_WDT_GLB_SRST_EN_SHIFT) | (1 << CRU_GLB_RST_CON_WDT_GLB_SRST_CTRL_SHIFT);
+        break;
+    case GLB_RST_SND_WDT0:
+        mask = CRU_GLB_RST_CON_WDT_GLB_SRST_EN_MASK | CRU_GLB_RST_CON_WDT_GLB_SRST_CTRL_MASK;
+        val = (1 << CRU_GLB_RST_CON_WDT_GLB_SRST_EN_SHIFT);
+        break;
+    default:
+
+        return HAL_INVAL;
+    }
+
+    CRU->GLB_RST_CON = VAL_MASK_WE(mask, val);
+
     return HAL_OK;
 }
 
