ff035a173dc08044c76438e7302a54e0dcf323bf
Steven Liu <steven.liu@rock-chips.com>
2021-01-30 10:31:04 +0800

    lib: bsp: rk3568: Add support for rk3568
    Signed-off-by: Steven Liu <steven.liu@rock-chips.com>
    Change-Id: I13c980a585bcd281b0dd60262ee5de3109e91e7f

--- lib/bsp/RK3568/hal_bsp.c
+++ lib/bsp/RK3568/hal_bsp.c
@@ -1,6 +1,22 @@
 /* SPDX-License-Identifier: BSD-3-Clause */
 /*
- * Copyright (c) 2020 Rockchip Electronics Co., Ltd.
+ * Copyright (c) 2021 Rockchip Electronics Co., Ltd.
  */
 
 #include "hal_bsp.h"
+
+#ifdef HAL_UART_MODULE_ENABLED
+const struct HAL_UART_DEV g_uart2Dev =
+{
+    .pReg = UART2,
+    .sclkID = 0,//CLK_UART2,
+    .sclkGateID = 0,//CLK_UART2_PMU_GATE,
+    .pclkGateID = 0,//PCLK_PMU_UART2_GATE,
+    .irqNum = UART2_IRQn,
+    .isAutoFlow = false,
+};
+#endif
+
+void BSP_Init(void)
+{
+}
