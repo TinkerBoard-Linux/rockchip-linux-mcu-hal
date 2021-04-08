7c9539d0655b45073903b9bd69d7377cf600ad63
Tony Xie <tony.xie@rock-chips.com>
2021-04-08 09:38:05 +0800

    project: rk3568: support gpio irq group.
    Signed-off-by: Tony Xie <tony.xie@rock-chips.com>
    Change-Id: Id2cc2e94fc4aafb9fc5184143102ea23be31e946

--- project/rk3568/src/main.c
+++ project/rk3568/src/main.c
@@ -8,6 +8,19 @@
 #include "unity_runner.h"
 
 /********************* Private MACRO Definition ******************************/
+#define LEVEL0_CPU0_GPIO_BITS (0) /* GPIO_PIN_A0 | GPIO_PIN_A1 */
+#define LEVEL0_CPU1_GPIO_BITS (0)
+#define LEVEL0_CPU2_GPIO_BITS (0)
+#define LEVEL0_CPU3_GPIO_BITS (0)
+
+#define LEVEL1_CPU0_GPIO_BITS (0)
+#define LEVEL1_CPU1_GPIO_BITS (0)
+#define LEVEL1_CPU2_GPIO_BITS (0)
+#define LEVEL1_CPU3_GPIO_BITS (0)
+
+#define LEVEL2_CPU0_GPIO_BITS (0)
+#define LEVEL2_CPU1_GPIO_BITS (0)
+#define LEVEL2_CPU2_GPIO_BITS (0)
 
 /********************* Private Structure Definition **************************/
 
@@ -23,6 +36,63 @@ static struct GIC_IRQ_AMP_CTRL irqConfig = {
     .irqsCfg = &irqsConfig[0],
 };
 
+static const struct GPIO_IRQ_GROUP_CFG gpioIrqCfg[GPIO_BANK_NUM] = {
+    [GPIO_BANK0] = {
+        GPIO_IRQ_GROUP_GPIO_CFG(GPIO_BANK0, GPIO0_IRQn, GPIO0),
+        .groupIrqEn = GPIO_IRQ_GROUP_EN_BANK_TYPE,
+        .bankTypeCfg = {
+            .hwIrqCpuAff = CPU_GET_AFFINITY(1, 0),
+            .prio = 0xd0,
+        },
+    },
+    [GPIO_BANK1] = {
+        GPIO_IRQ_GROUP_GPIO_CFG(GPIO_BANK1, GPIO1_IRQn, GPIO1),
+        .groupIrqEn = GPIO_IRQ_GROUP_EN_BANK_TYPE,
+        .bankTypeCfg = {
+            .hwIrqCpuAff = CPU_GET_AFFINITY(1, 0),
+            .prio = 0xd0,
+        },
+    },
+    [GPIO_BANK2] = {
+        GPIO_IRQ_GROUP_GPIO_CFG(GPIO_BANK2, GPIO2_IRQn, GPIO2),
+        .groupIrqEn = GPIO_IRQ_GROUP_EN_BANK_TYPE,
+        .bankTypeCfg = {
+            .hwIrqCpuAff = CPU_GET_AFFINITY(1, 0),
+            .prio = 0xd0,
+        },
+        .prioGroup[0] = {
+            .cpuGroup = { LEVEL0_CPU0_GPIO_BITS, LEVEL0_CPU1_GPIO_BITS,
+                          LEVEL0_CPU2_GPIO_BITS, LEVEL0_CPU3_GPIO_BITS },
+            .GIRQId = { RSVD_IRQn(39), GPIO2_IRQn,
+                        RSVD_IRQn(40), RSVD_IRQn(41) },
+            .prio = 0x80,
+        },
+
+        .prioGroup[1] = {
+            .cpuGroup = { LEVEL1_CPU0_GPIO_BITS, LEVEL1_CPU1_GPIO_BITS,
+                          LEVEL1_CPU2_GPIO_BITS, LEVEL1_CPU3_GPIO_BITS },
+            .GIRQId = { RSVD_IRQn(42), RSVD_IRQn(43),
+                        RSVD_IRQn(44), RSVD_IRQn(45) },
+            .prio = 0x90,
+        },
+
+        .prioGroup[2] = {
+            .cpuGroup = { LEVEL2_CPU0_GPIO_BITS, LEVEL2_CPU1_GPIO_BITS,
+                          LEVEL2_CPU2_GPIO_BITS, 0 },
+            .GIRQId = { RSVD_IRQn(46), RSVD_IRQn(47),
+                        RSVD_IRQn(48), RSVD_IRQn(49) },
+            .prio = 0xa0,
+        },
+    },
+    [GPIO_BANK3] = {
+        GPIO_IRQ_GROUP_GPIO_CFG(GPIO_BANK3, GPIO3_IRQn, GPIO3),
+        .groupIrqEn = GPIO_IRQ_GROUP_EN_BANK_TYPE,
+        .bankTypeCfg = {
+            .hwIrqCpuAff = CPU_GET_AFFINITY(1, 0),
+            .prio = 0xd0,
+        },
+    },
+};
 /********************* Private Function Definition ***************************/
 
 /********************* Public Function Definition ****************************/
@@ -87,6 +157,9 @@ void main(void)
     /* UART Init */
     HAL_UART_Init(&g_uart2Dev, &hal_uart_config);
     HAL_GIC_Init(&irqConfig);
+    HAL_GPIO_IRQ_GROUP_Init(CPU_GET_AFFINITY(1, 0),
+                            gpioIrqCfg,
+                            HAL_IRQ_HANDLER_GetGpioIrqGroupOps());
     printf("Hello RK3568 Bare-metal using RK_HAL!\n");
 
     /* Unity Test */
