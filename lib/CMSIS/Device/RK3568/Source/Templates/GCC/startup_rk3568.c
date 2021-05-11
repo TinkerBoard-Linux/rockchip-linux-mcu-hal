7c326677bcfc5dbec25901d8026905e0606e3c36
Steven Liu <steven.liu@rock-chips.com>
2021-05-11 16:48:58 +0800

    Device: RK3568: modify startup_rk3568.c coding style
    Signed-off-by: Steven Liu <steven.liu@rock-chips.com>
    Change-Id: Iee5a14745cc84a4dc4b3b514a8d945cdbf39d114

--- lib/CMSIS/Device/RK3568/Source/Templates/GCC/startup_rk3568.c
+++ lib/CMSIS/Device/RK3568/Source/Templates/GCC/startup_rk3568.c
@@ -8,12 +8,12 @@
 /*----------------------------------------------------------------------------
   Internal References
  *----------------------------------------------------------------------------*/
-void Vectors       (void) __attribute__ ((section("RESET")));
-void Reset_Handler (void);
-void Default_Handler(void);
-void IRQ_Handler(void);
-void IRQ_HardIrqHandler(void);
-void IRQ_HardIrqPreemptHandler(uint32_t irqn);
+void Vectors                   (void) __attribute__ ((section("RESET")));
+void Reset_Handler             (void);
+void Default_Handler           (void);
+void IRQ_Handler               (void);
+void IRQ_HardIrqHandler        (void);
+void IRQ_HardIrqPreemptHandler (uint32_t irqn);
 
 /*----------------------------------------------------------------------------
   Exception / Interrupt Handler
@@ -107,7 +107,8 @@ void IRQ_Handler(void)
 /*----------------------------------------------------------------------------
   Exception / Interrupt Vector Table
  *----------------------------------------------------------------------------*/
-void Vectors(void) {
+void Vectors(void)
+{
   __ASM volatile(
   "LDR    PC, =Reset_Handler                        \n"
   "LDR    PC, =Undef_Handler                        \n"
@@ -123,45 +124,46 @@ void Vectors(void) {
 /*----------------------------------------------------------------------------
   Reset Handler called on controller reset
  *----------------------------------------------------------------------------*/
-void Reset_Handler(void) {
+void Reset_Handler(void)
+{
   __ASM volatile(
 
   // Mask interrupts
   "CPSID   if                                      \n"
 
   // Reset SCTLR Settings
-  "MRC     p15, 0, R0, c1, c0, 0                   \n"  // Read CP15 System Control register
-  "BIC     R0, R0, #(0x1 << 12)                    \n"  // Clear I bit 12 to disable I Cache
-  "BIC     R0, R0, #(0x1 <<  2)                    \n"  // Clear C bit  2 to disable D Cache
-  "BIC     R0, R0, #0x1                            \n"  // Clear M bit  0 to disable MMU
-  "BIC     R0, R0, #(0x1 << 11)                    \n"  // Clear Z bit 11 to disable branch prediction
-  "BIC     R0, R0, #(0x1 << 13)                    \n"  // Clear V bit 13 to disable hivecs
-  "MCR     p15, 0, R0, c1, c0, 0                   \n"  // Write value back to CP15 System Control register
+  "MRC     p15, 0, R0, c1, c0, 0                   \n" // Read CP15 System Control register
+  "BIC     R0, R0, #(0x1 << 12)                    \n" // Clear I bit 12 to disable I Cache
+  "BIC     R0, R0, #(0x1 <<  2)                    \n" // Clear C bit  2 to disable D Cache
+  "BIC     R0, R0, #0x1                            \n" // Clear M bit  0 to disable MMU
+  "BIC     R0, R0, #(0x1 << 11)                    \n" // Clear Z bit 11 to disable branch prediction
+  "BIC     R0, R0, #(0x1 << 13)                    \n" // Clear V bit 13 to disable hivecs
+  "MCR     p15, 0, R0, c1, c0, 0                   \n" // Write value back to CP15 System Control register
   "ISB                                             \n"
 
   // Configure ACTLR
-  "MRC     p15, 0, r0, c1, c0, 1                   \n"  // Read CP15 Auxiliary Control Register
-  "ORR     r0, r0, #(1 <<  1)                      \n"  // Enable L2 prefetch hint (UNK/WI since r4p1)
-  "MCR     p15, 0, r0, c1, c0, 1                   \n"  // Write CP15 Auxiliary Control Register
+  "MRC     p15, 0, r0, c1, c0, 1                   \n" // Read CP15 Auxiliary Control Register
+  "ORR     r0, r0, #(1 <<  1)                      \n" // Enable L2 prefetch hint (UNK/WI since r4p1)
+  "MCR     p15, 0, r0, c1, c0, 1                   \n" // Write CP15 Auxiliary Control Register
 
   // Set Vector Base Address Register (VBAR) to point to this application's vector table
   "LDR    R0, =Vectors                             \n"
   "MCR    p15, 0, R0, c12, c0, 0                   \n"
 
   // Setup Stack for each exceptional mode
-  "CPS    #0x11                                    \n"  // FIQ
+  "CPS    #0x11                                    \n" // FIQ
   "LDR    SP, =Image$$FIQ_STACK$$ZI$$Limit         \n"
-  "CPS    #0x12                                    \n"  // IRQ
+  "CPS    #0x12                                    \n" // IRQ
   "LDR    SP, =Image$$IRQ_STACK$$ZI$$Limit         \n"
-  "CPS    #0x13                                    \n"  // SVC
+  "CPS    #0x13                                    \n" // SVC
   "LDR    SP, =Image$$SVC_STACK$$ZI$$Limit         \n"
-  "CPS    #0x17                                    \n"  // ABT
+  "CPS    #0x17                                    \n" // ABT
   "LDR    SP, =Image$$ABT_STACK$$ZI$$Limit         \n"
-  "CPS    #0x1B                                    \n"  // UND
+  "CPS    #0x1B                                    \n" // UND
   "LDR    SP, =Image$$UND_STACK$$ZI$$Limit         \n"
-  "CPS    #0x1F                                    \n"  // SYS
+  "CPS    #0x1F                                    \n" // SYS
   "LDR    SP, =Image$$SYS_STACK$$ZI$$Limit         \n"
-  "CPS    #0x13                                    \n"  // SVC
+  "CPS    #0x13                                    \n" // SVC
 
   // Call DataInit
   "BL     DataInit                                 \n"
@@ -180,6 +182,7 @@ void Reset_Handler(void) {
 /*----------------------------------------------------------------------------
   Default Handler for Exceptions / Interrupts
  *----------------------------------------------------------------------------*/
-void Default_Handler(void) {
+void Default_Handler(void)
+{
   while(1);
 }
