d1075855dc91f5a98e46aaf29c391f05ed1d4bee
Elaine Zhang <zhangqing@rock-chips.com>
2021-06-03 15:42:49 +0800

    lib: bsp: rk2106: add uart clk id
    Signed-off-by: Elaine Zhang <zhangqing@rock-chips.com>
    Change-Id: I878a2e7b03275f9ed2a733e2c4c2a6bc86a6fdc6

--- lib/bsp/RK2106/hal_bsp.c
+++ lib/bsp/RK2106/hal_bsp.c
@@ -10,6 +10,9 @@
 const struct HAL_UART_DEV g_uart0Dev =
 {
     .pReg = UART0,
+    .sclkID = CLK_UART0,
+    .sclkGateID = CLK_UART0_GATE,
+    .pclkGateID = PCLK_UART0_GATE,
     .irqNum = UART0_IRQn,
     .isAutoFlow = false,
 };
@@ -17,6 +20,9 @@ const struct HAL_UART_DEV g_uart0Dev =
 const struct HAL_UART_DEV g_uart1Dev =
 {
     .pReg = UART1,
+    .sclkID = CLK_UART1,
+    .sclkGateID = CLK_UART1_GATE,
+    .pclkGateID = PCLK_UART1_GATE,
     .irqNum = UART1_IRQn,
     .isAutoFlow = true,
 };
@@ -24,6 +30,9 @@ const struct HAL_UART_DEV g_uart1Dev =
 const struct HAL_UART_DEV g_uart2Dev =
 {
     .pReg = UART2,
+    .sclkID = CLK_UART2,
+    .sclkGateID = CLK_UART2_GATE,
+    .pclkGateID = PCLK_UART2_GATE,
     .irqNum = UART2_IRQn,
     .isAutoFlow = true,
 };
@@ -31,6 +40,9 @@ const struct HAL_UART_DEV g_uart2Dev =
 const struct HAL_UART_DEV g_uart3Dev =
 {
     .pReg = UART3,
+    .sclkID = CLK_UART3,
+    .sclkGateID = CLK_UART3_GATE,
+    .pclkGateID = PCLK_UART3_GATE,
     .irqNum = UART3_IRQn,
     .isAutoFlow = true,
 };
@@ -38,6 +50,9 @@ const struct HAL_UART_DEV g_uart3Dev =
 const struct HAL_UART_DEV g_uart4Dev =
 {
     .pReg = UART4,
+    .sclkID = CLK_UART4,
+    .sclkGateID = CLK_UART4_GATE,
+    .pclkGateID = PCLK_UART4_GATE,
     .irqNum = UART4_IRQn,
     .isAutoFlow = true,
 };
@@ -45,6 +60,9 @@ const struct HAL_UART_DEV g_uart4Dev =
 const struct HAL_UART_DEV g_uart5Dev =
 {
     .pReg = UART5,
+    .sclkID = CLK_UART5,
+    .sclkGateID = CLK_UART5_GATE,
+    .pclkGateID = PCLK_UART5_GATE,
     .irqNum = UART5_IRQn,
     .isAutoFlow = true,
 };
