91e5fd30d2a517ff976564fccdc9f22e2fbf1e28
Cliff Chen <cliff.chen@rock-chips.com>
2022-07-18 15:22:51 +0800

    lib: Device: Add copy table support for rk3308
    Signed-off-by: Cliff Chen <cliff.chen@rock-chips.com>
    Change-Id: I3891d58d2fcb1066a98f709c965d4992b2a7307f

--- lib/CMSIS/Device/RK3308/Source/Templates/system_rk3308.c
+++ lib/CMSIS/Device/RK3308/Source/Templates/system_rk3308.c
@@ -69,16 +69,32 @@ void SystemInit(void)
 void DataInit(void)
 {
     typedef struct {
-        unsigned long *dest;
-        unsigned long wlen;
+        uint32_t const* src;
+        uint32_t* dest;
+        uint32_t  wlen;
+    } __copy_table_t;
+
+    typedef struct {
+        uint32_t* dest;
+        uint32_t  wlen;
     } __zero_table_t;
 
+    extern const __copy_table_t __copy_table_start__;
+    extern const __copy_table_t __copy_table_end__;
     extern const __zero_table_t __zero_table_start__;
     extern const __zero_table_t __zero_table_end__;
+
+    for (__copy_table_t const* pTable = &__copy_table_start__; pTable < &__copy_table_end__; ++pTable) {
+        for(uint32_t i=0u; i<pTable->wlen; ++i) {
+            pTable->dest[i] = pTable->src[i];
+        }
+    }
 
     for (__zero_table_t const *pTable = &__zero_table_start__; pTable < &__zero_table_end__; ++pTable) {
         for (unsigned long i = 0u; i < pTable->wlen; ++i) {
             pTable->dest[i] = 0u;
         }
     }
+
+
 }
