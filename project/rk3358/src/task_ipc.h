dd27a938124ed6c27f620cee6ccdfcf6942aebdc
Cliff Chen <cliff.chen@rock-chips.com>
2023-01-30 15:22:05 +0800

    lib: CMSIS: Device: RK3358: Add initial version
    Change-Id: I3f3ac96189db82077ad2d07b468e5ebe0a9528d2
    Signed-off-by: Cliff Chen <cliff.chen@rock-chips.com>

---
+++ project/rk3358/src/task_ipc.h
@@ -0,0 +1,14 @@
+/* SPDX-License-Identifier: BSD-3-Clause */
+/*
+ * Copyright (c) 2023 Rockchip Electronics Co., Ltd.
+ */
+
+#ifndef _TASK_IPC_H_
+#define _TASK_IPC_H_
+
+//#define IPC_ENABLE
+
+/* spinlock ID define */
+#define RK_PRINTF_SPINLOCK_ID 0U
+
+#endif
