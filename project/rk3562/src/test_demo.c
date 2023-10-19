45ced7174ec4b6432b6a06ce8495e5e5ad0bfdda
Steven Liu <steven.liu@rock-chips.com>
2023-10-19 19:23:21 +0800

    project: rk3562: add rpmsg linux test
    Change-Id: I9f381be09a6140fbdda87d7cc5b76e1002d4d4c9
    Signed-off-by: Steven Liu <steven.liu@rock-chips.com>

--- project/rk3562/src/test_demo.c
+++ project/rk3562/src/test_demo.c
@@ -13,6 +13,7 @@
 /********************* Private MACRO Definition ******************************/
 //#define IRQ_LATENCY_TEST
 //#define PERF_TEST
+//#define RPMSG_LINUX_TEST
 //#define RPMSG_PERF_TEST
 //#define SOFTIRQ_TEST
 //#define SPINLOCK_TEST
@@ -27,8 +28,9 @@ static struct GIC_AMP_IRQ_INIT_CFG irqsConfig[] = {
  * GIC version: GICv2
  * The priority higher than 0x80 is non-secure interrupt.
  */
-    GIC_AMP_IRQ_CFG_ROUTE(UART0_IRQn, 0xd0, CPU_GET_AFFINITY(1, 0)),
+#ifndef RPMSG_LINUX_TEST
     GIC_AMP_IRQ_CFG_ROUTE(MBOX_AP_IRQn, 0xd0, CPU_GET_AFFINITY(0, 0)),
+#endif
     GIC_AMP_IRQ_CFG_ROUTE(MBOX_BB_IRQn, 0xd0, CPU_GET_AFFINITY(3, 0)),
 
 #ifdef IRQ_LATENCY_TEST
@@ -164,6 +166,131 @@ static void perf_test(void)
         free(ptr);
     }
     printf("test memset end!\n");
+}
+#endif
+
+/************************************************/
+/*                                              */
+/*              RPMSG_LINUX_TEST                */
+/*                                              */
+/************************************************/
+#ifdef RPMSG_LINUX_TEST
+#include "rpmsg_lite.h"
+#include "rpmsg_ns.h"
+
+// test is CPU0 as master and CPU3 as remote.
+#define MASTER_ID   ((uint32_t)0)
+#define REMOTE_ID_3 ((uint32_t)3)
+
+// define remote endpoint id for test
+#define RPMSG_HAL_REMOTE_TEST3_EPT      0x3003U
+#define RPMSG_HAL_REMOTE_TEST_EPT3_NAME "rpmsg-ap3-ch0"
+
+#define RPMSG_HAL_TEST_MSG "Rockchip rpmsg linux test!"
+
+/* TODO: These are defined in the linked script gcc_arm.ld.S */
+extern uint32_t __linux_share_rpmsg_start__[];
+extern uint32_t __linux_share_rpmsg_end__[];
+
+#define RPMSG_LINUX_MEM_BASE ((uint32_t)&__linux_share_rpmsg_start__)
+#define RPMSG_LINUX_MEM_END  ((uint32_t)&__linux_share_rpmsg_end__)
+#define RPMSG_LINUX_MEM_SIZE (2UL * RL_VRING_OVERHEAD)
+
+struct rpmsg_block_t {
+    uint32_t len;
+    uint8_t buffer[496 - 4];
+};
+
+struct rpmsg_info_t {
+    struct rpmsg_lite_instance *instance;
+    struct rpmsg_lite_endpoint *ept;
+    uint32_t cb_sta;    // callback status flags
+    void * private;
+    uint32_t m_ept_id;
+};
+
+static void rpmsg_share_mem_check(void)
+{
+    if ((RPMSG_LINUX_MEM_BASE + RPMSG_LINUX_MEM_SIZE) > RPMSG_LINUX_MEM_END) {
+        printf("shared memory size error!\n");
+        while (1) {
+            ;
+        }
+    }
+}
+
+rpmsg_ns_new_ept_cb rpmsg_ns_cb(uint32_t new_ept, const char *new_ept_name, uint32_t flags, void *user_data)
+{
+    uint32_t cpu_id;
+    char ept_name[RL_NS_NAME_SIZE];
+
+    cpu_id = HAL_CPU_TOPOLOGY_GetCurrentCpuId();
+    strncpy(ept_name, new_ept_name, RL_NS_NAME_SIZE);
+    printf("rpmsg remote: new_ept-0x%lx name-%s\n", new_ept, ept_name);
+}
+
+static int32_t remote_ept_cb(void *payload, uint32_t payload_len, uint32_t src, void *priv)
+{
+    int ret;
+    uint32_t cpu_id;
+    struct rpmsg_info_t *info = (struct rpmsg_info_t *)priv;
+    struct rpmsg_block_t *block = (struct rpmsg_block_t *)info->private;
+
+    cpu_id = HAL_CPU_TOPOLOGY_GetCurrentCpuId();
+
+    info->m_ept_id = src;
+    block->len = payload_len;
+    memcpy(block->buffer, payload, payload_len);
+    info->cb_sta = 1;
+    //printf("rpmsg remote: msg: %s\n", block->buffer);
+    ret = rpmsg_lite_send(info->instance, info->ept, info->m_ept_id, RPMSG_HAL_TEST_MSG, strlen(RPMSG_HAL_TEST_MSG), RL_BLOCK);
+
+    if (ret) {
+        printf("rpmsg_lite_send error!\n");
+    }
+
+    return ret;
+}
+
+static void rpmsg_linux_test(void)
+{
+    uint32_t master_id, remote_id;
+    struct rpmsg_info_t *info;
+    struct rpmsg_block_t *block;
+    uint32_t ept_flags;
+    void *ns_cb_data;
+
+    rpmsg_share_mem_check();
+    master_id = MASTER_ID;
+    remote_id = HAL_CPU_TOPOLOGY_GetCurrentCpuId();
+    printf("rpmsg remote: remote core cpu_id-%ld\n", remote_id);
+//    printf("rpmsg remote: shmem_base-0x%lx shmem_end-%lx\n", RPMSG_LINUX_MEM_BASE, RPMSG_LINUX_MEM_END);
+
+    info = malloc(sizeof(struct rpmsg_info_t));
+    if (info == NULL) {
+        printf("info malloc error!\n");
+        while (1) {
+            ;
+        }
+    }
+    info->private = malloc(sizeof(struct rpmsg_block_t));
+    if (info->private == NULL) {
+        printf("info malloc error!\n");
+        while (1) {
+            ;
+        }
+    }
+
+    info->instance = rpmsg_lite_remote_init((void *)RPMSG_LINUX_MEM_BASE, RL_PLATFORM_SET_LINK_ID(master_id, remote_id), RL_NO_FLAGS);
+    rpmsg_lite_wait_for_link_up(info->instance);
+    printf("rpmsg remote: link up! link_id-0x%lx\n", info->instance->link_id);
+    rpmsg_ns_bind(info->instance, rpmsg_ns_cb, &ns_cb_data);
+    info->ept = rpmsg_lite_create_ept(info->instance, RPMSG_HAL_REMOTE_TEST3_EPT, remote_ept_cb, info);
+    ept_flags = RL_NS_CREATE;
+    rpmsg_ns_announce(info->instance, info->ept, RPMSG_HAL_REMOTE_TEST_EPT3_NAME, ept_flags);
+    while (info->cb_sta != 1) {
+    }
+    rpmsg_lite_send(info->instance, info->ept, info->m_ept_id, RPMSG_HAL_TEST_MSG, sizeof(RPMSG_HAL_TEST_MSG), RL_BLOCK);
 }
 #endif
 
@@ -404,6 +531,10 @@ void test_demo(void)
 
 #if defined(PERF_TEST) && defined(CPU0)
     perf_test();
+#endif
+
+#if defined(RPMSG_LINUX_TEST) && defined(CPU3)
+    rpmsg_linux_test();
 #endif
 
 #ifdef RPMSG_PERF_TEST
