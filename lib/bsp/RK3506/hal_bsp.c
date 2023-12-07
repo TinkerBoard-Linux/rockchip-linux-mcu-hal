732b8d112c3138b599d0328280923a10dcac8c88
Damon Ding <damon.ding@rock-chips.com>
2023-12-07 10:41:05 +0800

    lib: hal: pwm: support for pwm v4
    In addition, fix .irqNum to .irqNum[0] in HAL_PWM_DEV
    for PWM v1.
    
    Change-Id: Iff2129253f88e95c080f73fc03d72ca9cff8bb3f
    Signed-off-by: Damon Ding <damon.ding@rock-chips.com>

--- lib/bsp/RK3506/hal_bsp.c
+++ lib/bsp/RK3506/hal_bsp.c
@@ -167,7 +167,7 @@ const struct HAL_PWM_DEV g_pwm0Dev =
     .clkID = CLK_PMU1_PWM0,
     .clkGateID = CLK_PMU1_PWM0_GATE,
     .pclkGateID = PCLK_PMU1_PWM0_GATE,
-    .irqNum = PWM0_IRQn,
+    .irqNum[0] = PWM0_IRQn,
 };
 
 const struct HAL_PWM_DEV g_pwm1Dev =
@@ -176,7 +176,7 @@ const struct HAL_PWM_DEV g_pwm1Dev =
     .clkID = CLK_PWM1_PERI,
     .clkGateID = CLK_PWM1_PERI_GATE,
     .pclkGateID = PCLK_PWM1_PERI_GATE,
-    .irqNum = PWM1_IRQn,
+    .irqNum[0] = PWM1_IRQn,
 };
 
 const struct HAL_PWM_DEV g_pwm2Dev =
@@ -185,7 +185,7 @@ const struct HAL_PWM_DEV g_pwm2Dev =
     .clkID = CLK_PWM2_PERI,
     .clkGateID = CLK_PWM2_PERI_GATE,
     .pclkGateID = PCLK_PWM2_PERI_GATE,
-    .irqNum = PWM2_IRQn,
+    .irqNum[0] = PWM2_IRQn,
 };
 
 const struct HAL_PWM_DEV g_pwm3Dev =
@@ -194,7 +194,7 @@ const struct HAL_PWM_DEV g_pwm3Dev =
     .clkID = CLK_PWM3_PERI,
     .clkGateID = CLK_PWM3_PERI_GATE,
     .pclkGateID = PCLK_PWM3_PERI_GATE,
-    .irqNum = PWM3_IRQn,
+    .irqNum[0] = PWM3_IRQn,
 };
 #endif  /* End of HAL_PWM_MODULE_ENABLED */
 
